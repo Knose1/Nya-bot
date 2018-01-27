8f2ef559f136318dbbca5c7bad9a2413dd63d414
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-01-27 19:58:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ client.on('message', message => {
             isbanned = true;
         }
     });
-	if (message.channel.name == 'nya-bot-vs' && isbanned = true) {
+	if (message.channel.name == 'nya-bot-vs' && isbanned == true) {
 	    message.author.sendMessage(message.author+' vous êtes ban du Virtual server et ne pouvez donc pas parler dans le VS');
 		message.delete(500)
                 .then(msg => console.log(`Message supprimé, raison: Virtual channel; Auteur: ${msg.author}`))
