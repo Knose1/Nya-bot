49397817adb2cbc28ace79f36f71f89d33e10c50
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-02-03 22:28:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -288,6 +288,7 @@ client.on('message', message => {
 	}
     //Si contient un attachement
     else if ((message.channel.name == 'nya-bot-vs' || (message.guild.id == "377892426569744387" && message.channel.name == "nya-bot-vs-log")) && (message.content.indexOf('--') == 0 || message.content.indexOf('//') == 0) && undefined != message.attachments) {
+        console.log(message.attachments);
         message.author.sendMessage('Voici l\'url de vos attachments :');
         message.attachments.forEach(attachment => {
             message.author.sendMessage(attachment.url);
