cb9bd29e68a21b91aa972ec9c9981ae2bc4bb9c3
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-02-03 20:33:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -300,22 +300,17 @@ client.on('message', message => {
     var vsIsImage = false;
     var wordsIndex = -1;
     var newwords = new Array();
+    var vsImage = new Array();
     words.forEach(word => {
         var wordsIndex = wordsIndex + 1 ;
         if (!vsIsImage && word.indexOf('//') == 0) {
             var vsImage = word.slice('//'.length).trim().split(/ +/g);
-            //console.log(vsImage);
-            //console.log(vsImage[0]);
-            //vsImage = vsImage[0];
-            //console.log(vsImage);
+            vsImage = vsImage[0];
             vsIsImage = true;
         }
         else if (!vsIsImage && word.indexOf('--') == 0) {
             var vsImage = word.slice('--'.length).trim().split(/ +/g);
-            //console.log(vsImage);
-            //console.log(vsImage[0]);
-            //vsImage = vsImage[0];
-            //console.log(vsImage);
+            vsImage = vsImage[0];
             vsIsImage = true;
         }
         if (!vsIsImage) {newwords[wordsIndex] = word;}
@@ -323,7 +318,7 @@ client.on('message', message => {
     
     if (vsIsImage) {
     words = newwords;
-    var vsmessage = words.join(' ') + "\n " + vsImage[0];
+    var vsmessage = words.join(' ') + "\n " + vsImage;
     } else {var vsmessage = words.join(' ');}
         //On créer un embed
         
