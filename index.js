6f0e987e19a19db950cf2a29fcecd0f62e66f5c5
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-02-04 15:53:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -173,9 +173,9 @@ client.on('message', message => {
             isbanned = true;
         }
     });
-	
+	console.log(message.content.length);
     //Si (Personne Ban)
-    else if (message.channel.name == 'nya-bot-vs' && isbanned == true  && message.author.id != mention  && message.author != botowner) {
+    if (message.channel.name == 'nya-bot-vs' && isbanned == true  && message.author.id != mention  && message.author != botowner) {
 	    message.author.sendMessage(message.author+' vous êtes ban du Virtual server et ne pouvez donc pas parler dans le VS');
 		message.delete(500)
                 .then(msg => console.log(`Message supprimé, raison: Virtual channel; Auteur: ${msg.author}`))
@@ -189,28 +189,30 @@ client.on('message', message => {
                 .then(msg => console.log(`Message supprimé, raison: Virtual channel; Auteur: ${msg.author}`))
                 .catch(console.error);
 	}
-    //Si pas de -- et pas de //
-	else if (
-                (
-                message.channel.name == 'nya-bot-vs' || 
-                    (
+    //Si (pas de -- et pas de //)
+    else if (
+                ( //Un channel nya!bot VS
+                message.channel.name == 'nya-bot-vs'
+                    || 
+                    ( //ou le serveur log de nya!bot
                     message.guild.id == "377892426569744387" && 
                     message.channel.name == "nya-bot-vs-log"
                     )
                 ) && 
                 (
+                //Si ce n'est pas nya!bot
                 message.author.id != mention &&
                     (
-                        (
+                        (//S'il n'y a pas d'attachement et qu'il n'y a pas le préfix du VS
                         message.attachments.size == 0 &&
                         message.content.indexOf('--') != 0 && 
                         message.content.indexOf('//') != 0 
-                        ) || 
-                        (
+                        )
+                          || 
+                        (//S'il y a au moin un attachement et un text mais que le txt n'as pas le préfix du VS
                         message.attachments.size != 0 &&
-                        message.content.length != 0 && 
-                        message.content.lenght < 2 &&  
-                        message.content.indexOf('--') != 0 && 
+                        message.content.length != 0 &&
+                        message.content.indexOf('--') != 0 &&
                         message.content.indexOf('//') != 0
                         )
                     )
@@ -222,10 +224,9 @@ client.on('message', message => {
                 .then(msg => console.log(`Message supprimé, raison: Virtual channel; Auteur: ${msg.author}`))
                 .catch(console.error);
 	}
-    
 	/*On envoie des messages en tant que nya!bot*/
     
-    if ((message.channel.name == 'nya-bot-vs' || (message.guild.id == "377892426569744387" && message.channel.name == "nya-bot-vs-log")) && (message.content.indexOf('--nya') == 0 || message.content.indexOf('--Nya') == 0 || message.content.indexOf('//nya') == 0 || message.content.indexOf('//Nya') == 0) && (message.author == botowner || undefined != isMod)) {
+    else if ((message.channel.name == 'nya-bot-vs' || (message.guild.id == "377892426569744387" && message.channel.name == "nya-bot-vs-log")) && (message.content.indexOf('--nya') == 0 || message.content.indexOf('--Nya') == 0 || message.content.indexOf('//nya') == 0 || message.content.indexOf('//Nya') == 0) && (message.author == botowner || undefined != isMod)) {
         if (message.content.indexOf('//') == 0){
             var args = message.content.slice('//'.length).trim().split(/ +/g);
         }
