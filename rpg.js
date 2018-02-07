204a44a30437338b46bbd4a00753b6702a0d501b
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-02-08 00:05:48 +0100

    Update rpg.js

--- rpg.js
+++ rpg.js
@@ -1,1 +1,6 @@
-
+//Si c'est une commande, récupérer les arguments
+if (message.content.indexOf(prefix) == 0) {
+  var iscommand = true;
+  var args = message.content.slice(prefix.length).trim().split(/ +/g);
+  var command = args.shift().toLowerCase();
+}
