25d053a368016f4c0fb9e27ef4f69ca4c879ff85
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-02-08 14:17:39 +0100

    Delete rpg.js

--- rpg.js
+++
@@ -1,9 +0,0 @@
-message.author.send('Le nya!bot n\'est pas encore prêt');
-return;
-
-//Si c'est une commande, récupérer les arguments
-if (message.content.indexOf('cat>') == 0) {
-  var iscommand = true;
-  var args = message.content.slice(prefix.length).trim().split(/ +/g);
-  var command = args.shift().toLowerCase();
-}
