2703d409a6206c0a2fabbdf3f7713b09501e8d6f
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-02-08 14:08:13 +0100

    Update rpg.js

--- rpg.js
+++ rpg.js
@@ -2,7 +2,7 @@ message.autho.send('Le nya!bot n\'est pas encore prêt');
 return;
 
 //Si c'est une commande, récupérer les arguments
-if (message.content.indexOf(prefix) == 0) {
+if (message.content.indexOf('cat>') == 0) {
   var iscommand = true;
   var args = message.content.slice(prefix.length).trim().split(/ +/g);
   var command = args.shift().toLowerCase();
