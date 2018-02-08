a923676119b7331f600b788c4b76c59c3f49d164
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-02-08 16:07:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -836,15 +836,19 @@ client.on('message', message => {
                 (betaTest == 'off') ||
                 (betaTest == 'on' && isBTest) //Si le RPG est en vertion Test il faut être Béta testeur
             ) {
-            message.author.send('Le nya!bot n\'est pas encore prêt');
-            return;
 
-            //Si c'est une commande, récupérer les arguments
+            //on récupère les arguments
             if (message.content.indexOf('cat>') == 0) {
                 var iscommand = true;
-                var args = message.content.slice(prefix.length).trim().split(/ +/g);
+                var args = message.content.slice('cat>'.length).trim().split(/ +/g);
                 var command = args.shift().toLowerCase();
             }
+            
+            /*J'ai pas de compte 0.0 INSCRIT TOI ESCLAVE ! xD*/
+            
+            
+            
+            /*Fin de "J'ai pas de compte"*/
 
         } else {
             message.author.send('Désolé le RPG nya!bot est en vertion béta test');
