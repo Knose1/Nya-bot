6caada3478529794512d27b7aab0b96dcdfaf777
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-02-11 00:42:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -994,7 +994,7 @@ console.log(Database(${arg1}).get(${arg1}[0],'${arg2}',${arg3}));\`\`\`\n\
                     }
                 }
                 else {
-                message.user.send('use of `cat-db` : \n\
+                message.author.send('use of `cat-db` : \n\
     `cat-db <array1> <string> [array2]`');
                 }
             }
