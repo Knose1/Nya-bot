1fb60baeaeb0c07634bba5bca4b0361dc9f8e6e7
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-02-20 20:52:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,165 +21,7 @@ var noGame = 'activé'; // #no-Game-No-Life ! xDDD joke
 var betaTest = 'on';
 
 
-/*Test the db*/
 
-function TestDatabase(allRolePrefix, gt) {
-    //Si on a donner une liste de prefix
-    if (Array.isArray(allRolePrefix) && allRolePrefix.length > 0) {
-        let retError = '';
-        let toReturn = {};
-        //Pour chaque préfix
-        //console.log("allRolePrefix = "+allRolePrefix);
-        let noError = true;
-        allRolePrefix.forEach(rolePrefix => {
-            //console.log("rolePrefix = "+rolePrefix);
-            
-            
-            //S'il y a pas d'erreur:
-            if (noError) {
-                
-                //On regarde si le préfix est un txt
-                if (typeof(rolePrefix) == 'string') {
-                    var newRolePrefix = rolePrefix;
-                    toReturn[rolePrefix.replace(/:/g, "")] = new Array();
-                    rolePrefix = newRolePrefix;
-                    //On récupère les data de chaque role
-                    client.guilds.get('407142766674575361').roles.forEach(role => {
-                        //On regarde si le role correspond au préfix
-                        if (role.name.indexOf(rolePrefix) == 0) {
-                            //On récupère les data `${prefix}${data0} ${data1} ${data2}` exemple: user:1 1000
-                            var data = role.name.slice(rolePrefix.length).trim().split(/ +/g);
-                            toReturn[rolePrefix.replace(/:/g, "")][data[0]] = data.slice(1);
-                            //Résultat: toReturn[prefix (sans ":")][data0] = [data1, data2]; exemple: toReturn[user][1] = [1000]
-                        }
-                    });
-                    if (toReturn[rolePrefix.replace(/:/g, "")].length == 0) {
-                        noError = false;
-                        toReturn = undefined;
-                        retError += `Error: ${rolePrefix} not found in the db` + "\n";
-                    }
-                } else {
-                    //Si le préfix est pas un txt on retourne une erreur
-                    noError = false;
-                    toReturn = undefined;
-                    retError += `Not a string at allRolePrefix.forEach(role =>{}) && role = ${rolePrefix}`+ "\n";
-                    return [undefined, retError];
-                }
-            } else return [undefined, retError];
-        });
-        //On a récupéré les data de toReturn mais on a pas encors crée de méthode pour obtenir ${data0} à partir de ${data1} pour chaque préfix
-        if (retError == '' && 'noGet' != gt && 'noFunction' != gt) {
-            toReturn.get = function (dataPrefix, data1, prefixInclude) {
-                let retError = '';
-                let toBeReturned = {};
-                if (typeof(dataPrefix) == 'string' && typeof(data1) == 'string') {
-                    if (undefined != toReturn[dataPrefix.replace(/:/g, "")] ) {
-                        //On récupère l'id de la data à partir de la primary (dataPrefix)
-                        var id = toReturn[dataPrefix.replace(/:/g, "")].findIndex(data => {
-                            return data1 == data;
-                        });
-                        if (id != -1) {
-                            //On a donné une liste de préfixInclude
-                            if (prefixInclude != undefined && Array.isArray(prefixInclude)) {
-                                //Pour chaque préfix inclue
-                                toBeReturned['id'] = id;
-                                prefixInclude.forEach(prefixI => {
-                                    var defautprefixI = prefixI;
-                                    //Si toReturn contient le préfix et que le préfix n'est pas id
-                                    if (undefined != toReturn[prefixI.replace(/:/g, "")] && 'id' != toReturn[prefixI.replace(/:/g, "")]) {
-                                        //On récupère la data correspondant à l'id
-                                        toBeReturned[prefixI.replace(/:/g, "")] = {};
-                                        toBeReturned[prefixI.replace(/:/g, "")].value = toReturn[prefixI.replace(/:/g, "")][id];
-                                        
-                                        
-                                        if ('noSet' != gt && gt != 'noFunction') {
-                                            /*On créer une fonction .set()*/
-                                            toBeReturned[prefixI.replace(/:/g, "")].set = function (newValue) {
-                                                let retError = '';
-                                                if (Array.isArray(newValue)) {
-                                                    var datas = toReturn[prefixI.replace(/:/g, "")][id];
-                                                    client.guilds.get('407142766674575361').roles.find('name', defautprefixI+id+" "+datas.join(' ')).setName(defautprefixI+id+" "+newValue.join(' '))
-                                                        .catch(console.error);
-                                                    retError = `Edited the data "${defautprefixI+id+" "+datas.join(' ')}" => "${defautprefixI+id+" "+newValue.join(' ')}"` + "\n";
-                                                    return retError;
-                                                }
-                                                else {
-                                                    retError += `Not an Array at Database().get().set(${newValue})` + "\n";
-                                                    return retError;
-                                                }
-                                            }
-                                            /*FIN DE FONCTION SET*/
-                                        }
-                                    
-                                    
-                                    }
-                                    //Si le préfix est 'id'
-                                    else if('id' == prefixI.replace(/:/g, "")) {
-                                        retError += `The prefix 'id' is disable at Database().get()` + "\n";
-                                    }
-                                    //Sinon
-                                    else {
-                                        toBeReturned[prefixI.replace(/:/g, "")] = undefined;
-                                        retError += `toBeReturned[${prefixI.replace(/:/g, "")}] = undefined` + "\n";
-                                    }
-                                });
-                                return [toBeReturned, retError];
-                            }
-                            //On a pas donné de préfixInclude
-                            else if (prefixInclude == undefined) {
-                                retError += `prefixInclude is undefined at Database().get(${dataPrefix},${data1},${prefixInclude})`+"\n";
-                                return [undefined, retError];
-                            }
-                            //On a donné une var qui n'est pas une liste
-                            else if (prefixInclude != undefined && !Array.isArray(prefixInclude)) {
-                                retError += `Not and array at Database().get(${dataPrefix},${data1},${prefixInclude})`+"\n";
-                                return [undefined, retError];
-                            }
-                        } else {
-                            retError += `${data1} not found at Database().get(${dataPrefix},${data1})`+"\n";
-                            return [undefined, retError];
-                        }
-                    } else {
-                        retError += `Prefix unknown at Database().get(${dataPrefix})`+"\n";
-                        return [undefined, retError];
-                    }
-                } else {
-                    if (typeof(dataPrefix) != 'string') retError += `dataPrefix :Not a string at Database().get(${dataPrefix.toString()},${data1.toString()})`+"\n";
-                    if (typeof(data1) != 'string') retError += `data1 :Not a string at Database().get(${dataPrefix.toString()},${data1.toString()})`+"\n";
-                    return [undefined, retError];
-                }
-            };
-        }
-        /*Fonction Create*/
-        if (retError == '' && gt != 'noFunction' && gt != 'noCreate') {
-            toReturn.create = function (type,Wprefix,data) {
-                retError = '';
-                if (typeof(type) == 'string') {
-                    if(Array().isArray(Wprefix)) {
-                        client.guilds.get('407142766674575361').createRole({
-                            //name: 'Super Cool People',
-                            //color: 'BLUE',
-                        });
-                    } else {
-                    retError += `Not an array at Database().create(${type}, ${Wprefix})`;
-                    }
-                } else {
-                    retError += `Not a string at Database().create(${type})`;
-                }
-                return retError;
-            }
-        }
-        /*Fin de fonction create*/
-        return [toReturn, retError];
-    } else {
-        //Si on a pas donner de liste de préfix
-        if(!Array.isArray(allRolePrefix)) retError += `Not an array at 'db.new(${allRolePrefix.toString()})'`+"\n";
-        if(allRolePrefix.length <= 0) retError += `Can't read length < 0 at 'db.new(${allRolePrefix.toString()})'`+"\n";
-        return [undefined, retError];
-    }
-};
-
-/*End Test the db*/
 
 /*The DB PART 1*/
 function Database(SGuild, allRolePrefix, gt) {
@@ -337,23 +179,42 @@ function Database(SGuild, allRolePrefix, gt) {
         return [undefined, retError];
     }
 };
+/*Test the db*/
+
+function TestDatabase(allRolePrefix, gt) = Database__1('407142766674575361', allRolePrefix, gt);
+
+/*End Test the db*/
+
+
 
 /*The DB PART 2*/
 function Database(SGuild, allRolePrefix) {
     var x1 = Database__1(SGuild, allRolePrefix);
     if (x1[1] == '') {
         x1[0].get = function (dataPrefix, data1, prefixInclude) {
-            var x2 = Database__1(SGuild, allRolePrefix)[0].get;
-            if
+            var x2 = Database__1(SGuild, allRolePrefix)[0].get(dataPrefix, data1, prefixInclude)[0]
+            
+            if (x2[1] == '') {
+                
+                Database__1(SGuild, allRolePrefix)[0].get(dataPrefix, data1, prefixInclude)[0].forEach ( d => {
+                    x2[0].forEach( xn => {
+                        if (typeof(d) == 'object' && typeof(xn) == 'object' && d == xn) {
+                            return x2.set = function (newValue) { console.log(d.set(newValue)) };
+                        }
+                    });
+                });
+                return x2[0];
+            } else console.log(x2[0]);
+        
         };
             
         x1[0].create = function (type,Wprefix,data) {
-            var x2 = Database__1(SGuild, allRolePrefix)[0].get;
-            if 
+            var x2 = Database__1(SGuild, allRolePrefix)[0].create(type,Wprefix,data);
+            console.log(x2);
         };
-        return x1
-    }
-    else {console.log(x1[1]);}
+        
+        return x1[0];
+    } else console.log(x1[1]);
 };
 
 var catimg = new Array() ;
