baff638dcfb8533aa813faa7de2963fb3e72a8bb
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-02-23 15:08:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -471,23 +471,6 @@ client.on('resume', resume => {
 
 client.on('message', message => {
     
-    //On récupère le suffix du vs
-    var isVs = false;
-    if (message.channel.name.indexOf('nya-bot-vs') == 0) {
-        var Pfx = message.channel.name.slice('nya-bot-vs'.length);
-        if (Pfx == undefined) Pfx = '';
-        if (Pfx.indexOf('-') == 0) Pfx = Pfx.slice('-'.length);
-        if (Pfx == undefined) Pfx = '';
-        
-        var isPfx = false
-        VsPrefixs.forEach( p => {
-            if (Pfx == p) isPfx = true;
-        });
-        
-        if (isPfx) isVs = true;
-        console.log(isVs+" ; "+Pfx)
-    }
-    
     
     if (!message.author.bot){
     //Faux serveurs dans la DB
@@ -540,6 +523,23 @@ client.on('message', message => {
     
     if (noGame == 'activé') client.user.setGame(`cat:help | Nya!Bot est en marche, avec ${client.users.size} users, dans ${client.channels.size} salons et ${client.guilds.size} serveurs.`);
     if(message.guild) {} else return;
+    
+    //On récupère le suffix du vs
+    var isVs = false;
+    if (message.channel.name.indexOf('nya-bot-vs') == 0) {
+        var Pfx = message.channel.name.slice('nya-bot-vs'.length);
+        if (Pfx == undefined) Pfx = '';
+        if (Pfx.indexOf('-') == 0) Pfx = Pfx.slice('-'.length);
+        if (Pfx == undefined) Pfx = '';
+        
+        var isPfx = false
+        VsPrefixs.forEach( p => {
+            if (Pfx == p) isPfx = true;
+        });
+        
+        if (isPfx) isVs = true;
+        console.log(isVs+" ; "+Pfx)
+    }
     
     //On regarde si l'utilisateur est un modérateur
     var isMod = (client.guilds.get('377892426569744387').roles.get('407229590948413440').members.get(message.author.id) != undefined);
