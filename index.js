21440aa77439d1640015e3b0ff609c01c4456bdf
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-02-26 14:58:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1577,6 +1577,52 @@ TestDatabase(${arg1},'noSet').get(${arg1}[0],'${arg2}',${arg5})['${arg3}'].set($
             message.channel.send(nyachannels);
             }
         }
+        //Commande book
+        else if (command.toLowerCase() == 'book') {
+            message.channel.send('#Are you ready to get DM ? / Est-tu prêt à recevoir un MP ?',{code:'md'})
+            .then(m => {
+                m.react('✔').then(m2 => m.react('✖'));
+            
+                const filter = (reaction, user) => user.id == message.author.id
+                const collector = m.createReactionCollector(filter, {time:3600000});
+                collector.on('collect', reaction => {
+                    switch (reaction.emoji.name) {
+                        case '✔':
+                            message.author.send('#Langage ?', {code:'md'}).then(botDM => {
+                                botDM.react('🇫🇷').then(botdm2 => botDM.react('🇬🇧'));
+                                
+                                const filter2 = (reaction, user) => user.id == message.author.id
+                                const collector2 = botDM.createReactionCollector(filter2, {time:3600000});
+                                collector2.on('collect', reactionDM => {
+                                    switch (reactionDM.emoji.name) {
+                                        case '🇫🇷':
+                                            botDM.clearReactions();
+                                            
+                                            client.on('message', dm => {
+                                                if(!dm.guild && dm.author == message.author && dm.channel.messages.get(botDM)) {
+                                                dm.channel.send('Hi');
+                                                } else dm.channel.send('Nope');
+                                            });
+                                            break;
+                                        case '🇬🇧':
+                                            botDM.clearReactions();
+                                            
+                                
+                                    }
+                                collector2.on('end', e => {if (e.size == 0) {botDM.delete(500);}});
+                            });
+                            m.delete(500);
+                            collector.stop();
+                            break;
+                        case '✖':
+                            m.delete(500);
+                            collector.stop();
+                    }
+                });
+                collector.on('end', e => {if (e.size == 0) {m.delete(500);}});
+            }); 
+            
+        }
         //Commande eval
         else if (command.toLowerCase() == 'eval' && message.author != botowner) {
             message.reply('You can\'t use eval command ! :pouting_cat:')
