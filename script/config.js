2a207b479cc7084f8060b360634577d9078d62ac
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-02-28 00:41:18 +0100

    Update config.js

--- script/config.js
+++ script/config.js
@@ -1,12 +1,12 @@
 /*key is the bot's token; mention is the bot's id*; logserv is the server for bot log*/
-exports.key = ﻿process.env.TOKEN;
-exports.mention = ﻿process.env.BOTMENTION;
-exports.logserv = process.env.LOGSERV;
-exports.botowner = process.env.BOTW;
-exports.prefix ='cat:';
-exports.betaTest = 'on';
-
-exports.CanReloading = false;
-exports.noGame = 'activé';
-
-exports.VsPrefixs = ['','english','nsfw','french','test'];
+exports.load = () => {
+return 
+    key = ﻿process.env.TOKEN,
+    mention = ﻿process.env.BOTMENTION,
+    logserv = process.env.LOGSERV,botowner = process.env.BOTW,
+    prefix ='cat:',
+    betaTest = 'on',
+    CanReloading = false,
+    noGame = 'activé',
+    VsPrefixs = ['','english','nsfw','french','test'];
+}
