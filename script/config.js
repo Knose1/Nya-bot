856ad6bb6667dc63bc15afdfb58b63836cb05573
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-02-27 20:45:51 +0100

    Update config.js

--- script/config.js
+++ script/config.js
@@ -1,1 +1,12 @@
-let VsPrefixs = ['','english','nsfw','french','test'];
+/*key is the bot's token; mention is the bot's id*; logserv is the server for bot log*/
+var key = ﻿process.env.TOKEN;
+var mention = ﻿process.env.BOTMENTION;
+var logserv = process.env.LOGSERV;
+var botowner = process.env.BOTW;
+var prefix ='cat:';
+var betaTest = 'on';
+
+let CanReloading = false;
+let noGame = 'activé';
+
+var VsPrefixs = ['','english','nsfw','french','test'];
