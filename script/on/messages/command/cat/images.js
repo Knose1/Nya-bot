bd43ee18df4df0d87fbe5b4f701865386e48e363
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-02-27 21:11:11 +0100

    Update images.js

--- script/on/messages/command/cat/images.js
+++ script/on/messages/command/cat/images.js
@@ -1,1 +1,51 @@
+exports.load = max => {
 
+var catimg = new Array() ;
+    catimg = [
+        "https://yt3.ggpht.com/-xMN6CtD0oAM/AAAAAAAAAAI/AAAAAAAAAAA/3rguRakaom8/s900-c-k-no-mo-rj-c0xffffff/photo.jpg",
+        "https://wallpaper.wiki/wp-content/uploads/2017/05/wallpaper.wiki-Anime-Cat-Full-HD-Wallpaper-PIC-WPC0012460.jpg",
+        "https://i.pinimg.com/736x/a6/8b/fb/a68bfb3aa12707c10335a4b40611719e--black-cat-anime-anime-cat.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY6AnbOswJ5Z3UIAyMxgb-hO0l4X7fLz4e1YlmnLxjCsmUR5vq",
+        "http://www.flipcat.us/wp-content/uploads/2017/11/captivating-artwork-cat-anime-glowing-clouds-apofiss-wallpapers-hd-also-marvelous-cat-wallpaper-anime.jpg",
+        "https://pbs.twimg.com/profile_images/1798927817/Chi001.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRk4G02HepCrUM_JswsuADFZFf4OUyMrXyDpEF_7m-jNEpTJRgO",
+        "https://media.giphy.com/media/A6JOgQslWdYqI/giphy-facebook_s.jpg",
+        "https://myanimelist.cdn-dena.com/s/common/uploaded_files/1449565712-5aafae0c93dc1277f3b714736537eb1d.gif",
+        "https://i.imgur.com/vs2IkYB.gif",
+        "http://clipground.com/images/cute-anime-cat-clipart-19.jpg",
+        "https://wallpaper.wiki/wp-content/uploads/2017/04/wallpaper.wiki-Cute-Anime-Cat-Background-HQ-PIC-WPC001902.jpg",
+        "http://i.imgur.com/kkqZHi8.jpg",
+        "https://data.whicdn.com/images/21075753/original.png",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBvORlETA2KGz95tGZHAGti4TWphRYTHc-H14niFvHYk-3-tEK",
+        "https://i.pinimg.com/736x/0f/00/9b/0f009b24bb1c63a1bc56fd2580ea813c--chibi-kawaii-neko-girl-kawaii.jpg",
+        "https://i.ytimg.com/vi/4Ns47GgF4D4/maxresdefault.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwtWEOgHUdqyhR2LLkj3jstn0Q4uWyyJj7lizS018dyaFrCoFA",
+        "https://i.pinimg.com/736x/a3/a1/4a/a3a14a447d1fd26b0292d55c5441e92a--anime-cat-boy-neko-boy.jpg",
+        "https://media.tenor.com/images/88333dc3104dd8d20ad51b0c48d8149d/tenor.png",
+        "https://orig00.deviantart.net/0616/f/2014/025/d/c/anime_neko_girl_with_mouse_by_renxrin-d73nhhj.jpg",
+        "https://orig00.deviantart.net/ae61/f/2014/326/9/d/anime_chibi_neko_girl_5_by_taemin4ever-d87925n.png",
+        "https://ae01.alicdn.com/kf/HTB1bo5gIFXXXXbRXXXXq6xXFXXXG/Cosplay-Neko-Anime-fantaisie-parti-Costume-Set-Lolita-gants-en-peluche-chat-kitty-Paw-cadeau.jpg_640x640.jpg",
+        "http://www.kinyu-z.net/data/wallpapers/190/1368393.jpg",
+        "http://38.media.tumblr.com/55b3f960c1c65aa9fc5d3468c4a754ef/tumblr_nqbqy3lH9Z1uo3jaho1_500.gif",
+        "https://rainbowgram.files.wordpress.com/2015/01/6f858-10903668_1538496133081588_481229092_n.jpg",
+        "https://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=118655064",
+        "http://i0.kym-cdn.com/photos/images/original/000/936/630/24f.gif",
+        "https://data.whicdn.com/images/52275745/original.jpg",
+        "https://orig00.deviantart.net/1a7b/f/2017/296/7/7/neko_girl_chibi_by_mysticalily-dbrj1dj.jpg",
+        "https://orig00.deviantart.net/c15a/f/2016/286/b/a/c__inirvanna_by_nekogirl_san-d9femg4.png",
+        "http://i0.kym-cdn.com/photos/images/newsfeed/000/862/883/c6b.png",
+        "https://media1.tenor.com/images/9c93248d94cfc9fb4a6895f6f08c7b61/tenor.gif?itemid=7556300",
+        "https://data.whicdn.com/images/66559264/large.jpg"
+    ];
+    if (undefined == max || String(Number(max)) == "NaN");
+        max = 0;
+    
+    var i = 0;
+    while(i < max) {
+        i += 1;
+        catimg.push('J\'ai la flemme, si tu veux une image fait le toi même');
+    }
+    
+    return catimg;
+    
+} 
