7d1fe06c7939e507bca3c965e83a1d6caded45e2
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-02-27 22:47:52 +0100

    Update index.js

--- script/index.js
+++ script/index.js
@@ -21,52 +21,7 @@ client.on('ready', () => {
     channel.send(`Nya!Bot est en marche, avec ${client.users.size} users, dans ${client.channels.size} salons et ${client.guilds.size} servers.`);
     client.user.setGame(`cat:help | Nya!Bot est en marche, avec ${client.users.size} users, dans ${client.channels.size} salons et ${client.guilds.size} serveurs.`);
 
-    //Faux serveurs dans la DB
-    client.guilds.get('410520625728323595').roles.forEach( gBan => {
-        if (
-            gBan.name == '377892426569744387' || /*Log serv*/
-            gBan.name == '406926403628695556' || /*Nya!bot ban*/
-            gBan.name == '410520625728323595' || /*Nya!bot ban serv*/
-            gBan.name == '407142766674575361' || /*Nya!bot database*/
-            gBan.name == '375434568980758528' || /*Mon serveur*/
-            gBan.name == '375434568980758528' || /*La théière*/
-            (NaN == Number(gBan.name) && gBan.name != "Nya!Bot" && gBan.name != "new role" && gBan.name != "@everyone")
-            ){
-					
-                gBan.delete()
-                    .then(r => {
-                        console.log(`Deleted role ${r}; Raison: 'Ban interdit ou serveur inexistant'`);
-                        client.channels.get('410520814920794133').send(`Deleted role \`${r.name}\`; __**Raison:**__ 'Ban interdit ou serveur inexistant'`);
-                    })
-                    .catch(console.error);
-        }
-    });
-    
-    //Serveur banni
-        client.guilds.forEach(guild => {
-            client.guilds.get('410520625728323595').roles.forEach( gBan => {
-                if (
-                        gBan.name == '377892426569744387' || /*Log serv*/
-                        gBan.name == '406926403628695556' || /*Nya!bot ban*/
-                        gBan.name == '410520625728323595' || /*Nya!bot ban serv*/
-                        gBan.name == '407142766674575361' || /*Nya!bot database*/
-                        gBan.name == '375434568980758528' || /*Mon serveur*/
-                        gBan.name == '375434568980758528' || /*La théière*/
-                        (undefined == client.guilds.get(gBan.name) && gBan.name != "Nya!Bot" && gBan.name != "@everyone" && undefined != client.guilds.get('410520625728323595').roles.get(gBan.id))
-                   ){
-                }
-                else if (guild.available && guild.id == gBan.name && undefined != client.guilds.get('410520625728323595').roles.get(gBan.id) && undefined != client.guilds.get(gBan.name)) {
-                    guild.leave()
-                        .then(g => {
-                            console.log(`Left the guild ${g.name}; Raison: 'Ban'`);
-                            client.channels.get('410520814920794133').send(`Left the guild \`${g.name}\`; __**Raison:**__ 'Ban'`);
-                            })
-                        .catch(console.error);
-                }
-            });
-        });
-    
-    //Fin de serveur banni
+    import '/modules/servban.js';
     
     client.channels.get('406802264540315648').send(`--Nya!Bot est en marche, avec ${client.users.size} users, dans ${client.channels.size} salons et ${client.guilds.size} servers.`);
     
@@ -78,52 +33,7 @@ client.on("guildCreate", guild => {
   var channel = client.channels.get(logserv);
   channel.send(`Nouveau serv: ${guild.name} (id: ${guild.id}). Nmb de membres: ${guild.memberCount}`);
   
-  //Faux serveurs dans la DB
-    client.guilds.get('410520625728323595').roles.forEach( gBan => {
-        if (
-            gBan.name == '377892426569744387' || /*Log serv*/
-            gBan.name == '406926403628695556' || /*Nya!bot ban*/
-            gBan.name == '410520625728323595' || /*Nya!bot ban serv*/
-            gBan.name == '407142766674575361' || /*Nya!bot database*/
-            gBan.name == '375434568980758528' || /*Mon serveur*/
-            gBan.name == '375434568980758528' || /*La théière*/
-            (NaN == Number(gBan.name) && gBan.name != "Nya!Bot" && gBan.name != "new role" && gBan.name != "@everyone")
-            ){
-					
-                gBan.delete()
-                    .then(r => {
-                        console.log(`Deleted role ${r}; Raison: 'Ban interdit ou serveur inexistant'`);
-                        client.channels.get('410520814920794133').send(`Deleted role \`${r.name}\`; __**Raison:**__ 'Ban interdit ou serveur inexistant'`);
-                    })
-                    .catch(console.error);
-        }
-    });
-    
-    //Serveur banni
-        client.guilds.forEach(guild => {
-            client.guilds.get('410520625728323595').roles.forEach( gBan => {
-                if (
-                        gBan.name == '377892426569744387' || /*Log serv*/
-                        gBan.name == '406926403628695556' || /*Nya!bot ban*/
-                        gBan.name == '410520625728323595' || /*Nya!bot ban serv*/
-                        gBan.name == '407142766674575361' || /*Nya!bot database*/
-                        gBan.name == '375434568980758528' || /*Mon serveur*/
-                        gBan.name == '375434568980758528' || /*La théière*/
-                        (undefined == client.guilds.get(gBan.name) && gBan.name != "Nya!Bot" && gBan.name != "@everyone" && undefined != client.guilds.get('410520625728323595').roles.get(gBan.id))
-                   ){
-                }
-                else if (guild.available && guild.id == gBan.name && undefined != client.guilds.get('410520625728323595').roles.get(gBan.id) && undefined != client.guilds.get(gBan.name)) {
-                    guild.leave()
-                        .then(g => {
-                            console.log(`Left the guild ${g.name}; Raison: 'Ban'`);
-                            client.channels.get('410520814920794133').send(`Left the guild \`${g.name}\`; __**Raison:**__ 'Ban'`);
-                            })
-                        .catch(console.error);
-                }
-            });
-        });
-    
-    //Fin de serveur banni
+  import '/modules/servban.js';
 });
 
 //Lorsqu'il a été kick d'un serv
@@ -216,52 +126,7 @@ client.on('message', message => {
     
     
     if (!message.author.bot){
-    //Faux serveurs dans la DB
-    client.guilds.get('410520625728323595').roles.forEach( gBan => {
-        if (
-            gBan.name == '377892426569744387' || /*Log serv*/
-            gBan.name == '406926403628695556' || /*Nya!bot ban*/
-            gBan.name == '410520625728323595' || /*Nya!bot ban serv*/
-            gBan.name == '407142766674575361' || /*Nya!bot database*/
-            gBan.name == '375434568980758528' || /*Mon serveur*/
-            gBan.name == '375434568980758528' || /*La théière*/
-            (NaN == Number(gBan.name) && gBan.name != "Nya!Bot" && gBan.name != "new role" && gBan.name != "@everyone")
-            ){
-					
-                gBan.delete()
-                    .then(r => {
-                        console.log(`Deleted role ${r}; Raison: 'Ban interdit ou serveur inexistant'`);
-                        client.channels.get('410520814920794133').send(`Deleted role \`${r.name}\`; __**Raison:**__ 'Ban interdit ou serveur inexistant'`);
-                    })
-                    .catch(console.error);
-        }
-    });
-    
-    //Serveur banni
-        client.guilds.forEach(guild => {
-            client.guilds.get('410520625728323595').roles.forEach( gBan => {
-                if (
-                        gBan.name == '377892426569744387' || /*Log serv*/
-                        gBan.name == '406926403628695556' || /*Nya!bot ban*/
-                        gBan.name == '410520625728323595' || /*Nya!bot ban serv*/
-                        gBan.name == '407142766674575361' || /*Nya!bot database*/
-                        gBan.name == '375434568980758528' || /*Mon serveur*/
-                        gBan.name == '375434568980758528' || /*La théière*/
-                        (undefined == client.guilds.get(gBan.name) && gBan.name != "Nya!Bot" && gBan.name != "@everyone" && undefined != client.guilds.get('410520625728323595').roles.get(gBan.id))
-                   ){
-                }
-                else if (guild.available && guild.id == gBan.name && undefined != client.guilds.get('410520625728323595').roles.get(gBan.id) && undefined != client.guilds.get(gBan.name)) {
-                    guild.leave()
-                        .then(g => {
-                            console.log(`Left the guild ${g.name}; Raison: 'Ban'`);
-                            client.channels.get('410520814920794133').send(`Left the guild \`${g.name}\`; __**Raison:**__ 'Ban'`);
-                            })
-                        .catch(console.error);
-                }
-            });
-        });
-    
-    //Fin de serveur banni
+    import '/modules/servban.js';
     }
     if(message.guild) {} else return;
     
