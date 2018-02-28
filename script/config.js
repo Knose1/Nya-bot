e11f209940174a61a8059117cb8e7ab2cfe8a62d
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-02-28 02:08:06 +0100

    Update config.js

--- script/config.js
+++ script/config.js
@@ -1,7 +1,6 @@
 /*key is the bot's token; mention is the bot's id*; logserv is the server for bot log*/
 exports.load = () => {
-return 
-    key = ﻿process.env.TOKEN,
+return key = ﻿process.env.TOKEN,
     mention = ﻿process.env.BOTMENTION,
     logserv = process.env.LOGSERV,botowner = process.env.BOTW,
     prefix ='cat:',
