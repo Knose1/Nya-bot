a6eb216c478911520e2419c5573908fb9ca5bc0b
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-02-28 12:57:16 +0100

    Update perm.js

--- script/module/perm.js
+++ script/module/perm.js
@@ -1,8 +1,12 @@
-//On regarde si l'utilisateur est un modérateur
-var isMod = (client.guilds.get('377892426569744387').roles.get('407229590948413440').members.get(message.author.id) != undefined);
+exports.load = () => {
+  //On regarde si l'utilisateur est un modérateur
+  var isMod1 = (client.guilds.get('377892426569744387').roles.get('407229590948413440').members.get(message.author.id) != undefined);
 
-//On regarde si l'utilisateur est un Béta testeur
-var isBTest = (client.guilds.get('377892426569744387').roles.get('410495831360143362').members.get(message.author.id) != undefined);
+  //On regarde si l'utilisateur est un Béta testeur
+  var isBTest1 = (client.guilds.get('377892426569744387').roles.get('410495831360143362').members.get(message.author.id) != undefined);
 
-//On regarde si l'utilisateur est un développeur
-var isADev = (client.guilds.get('377892426569744387').roles.get('407185267330514944').members.get(message.author.id) != undefined);
+  //On regarde si l'utilisateur est un développeur
+  var isADev1 = (client.guilds.get('377892426569744387').roles.get('407185267330514944').members.get(message.author.id) != undefined);
+  
+  return isMod = isMod1, isBTest = isBTest1, isADev = isADev1;
+}
