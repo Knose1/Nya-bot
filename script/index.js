ca91804ee51c4c6442d0645ffb71d5a23a60944b
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-02-28 13:04:07 +0100

    Update index.js

--- script/index.js
+++ script/index.js
@@ -134,7 +134,7 @@ client.on('message', message => {
     if (noGame == 'activé' && !iscommand) 
         client.user.setGame(`cat:help | Nya!Bot est en marche, avec ${client.users.size} users, dans ${client.channels.size} salons et ${client.guilds.size} serveurs.`);
     
-    require('./modules/perm.js').load();
+    require('./module/perm.js').load();
     
     
     
