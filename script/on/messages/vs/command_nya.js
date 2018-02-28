e7893f94bbcd63d817119f0f28e27d564f742601
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-02-28 20:59:49 +0100

    Create command_nya.js

---
+++ script/on/messages/vs/command_nya.js
@@ -0,0 +1,11 @@
+exports.execute = () => {
+    //Si (Commande Nya!bot)
+    if ((isVs || (message.guild.id == "377892426569744387" && message.channel.name == "nya-bot-vs-log")) && iscommand == true) {
+        message.author.send(message.author+' les commandes sont interdits dans se channel');
+        message.delete(500)
+                .then(msg => console.log(`Message supprimé, raison: Virtual channel; Auteur: ${msg.author}`))
+                .catch(console.error);
+        return true;
+    }
+    return false;
+}
