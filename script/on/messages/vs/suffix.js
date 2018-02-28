c03621e3f5fd6b5a7b298af4cb41e055b409c1b6
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-02-28 16:27:29 +0100

    Update suffix.js

--- script/on/messages/vs/suffix.js
+++ script/on/messages/vs/suffix.js
@@ -1,18 +1,22 @@
-//On récupère le suffix du vs
-var isVs = false;
-if (message.channel.name.indexOf('nya-bot-vs') == 0) {
-    var Pfx = message.channel.name.slice('nya-bot-vs'.length);
-    if (Pfx == undefined) Pfx = '';
-    if (Pfx.indexOf('-') == 0) Pfx = Pfx.slice('-'.length);
-    if (Pfx == undefined) Pfx = '';
+exports.then = (func) => {
+    //On récupère le suffix du vs
+    var isVs = false;
+    if (message.channel.name.indexOf('nya-bot-vs') == 0) {
+        var Pfx = message.channel.name.slice('nya-bot-vs'.length);
+        if (Pfx == undefined) Pfx = '';
+        if (Pfx.indexOf('-') == 0) Pfx = Pfx.slice('-'.length);
+        if (Pfx == undefined) Pfx = '';
     
-    var isPfx = false
-    VsPrefixs.forEach( p => {
-        if (Pfx == p) isPfx = true;
-    });
+        var isPfx = false
+        VsPrefixs.forEach( p => {
+            if (Pfx == p) isPfx = true;
+        });
     
-    if (isPfx) 
-        isVs = true;
+        if (isPfx) 
+            isVs = true;
     
-    //console.log(isVs+" ; "+Pfx);
+        //console.log(isVs+" ; "+Pfx);
+    }
+    
+    func();
 }
