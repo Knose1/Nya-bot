8abffda5420343b3caab6f984fe9959d753c4fc3
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-01 01:03:25 +0100

    Update index.js

--- script/on/messages/vs/index.js
+++ script/on/messages/vs/index.js
@@ -2,25 +2,16 @@ exports.execute = (message) => {
     
     var vsExecuted = false;
     
-    //On execute suffix.js pour récupérer le suffix du channel nya-bot-vs
-    require("./suffix.js").then( () => {
-    
-        //On execute isbanned.js pour savoir s'il est ban ou non
-        require("./isbanned.js").then( () => {
-        
-            //On passe aux conditions (soit on execute ... soit ... etc etc):
+    //On passe aux conditions (soit on execute ... soit ... etc etc):
             
-            if (require("./error_userBan.js").execute() ) vsExecuted = true; 
-            else if (require("./error_command.js").execute()  ) vsExecuted = true;
-            else if (require("./error_noPfix.js").execute() ) vsExecuted = true;
-            else if (require("./command_nya.js").execute()  ) vsExecuted = true;
-            else if (require("./command_purge.js").execute()    ) vsExecuted = true;
-            else if (require("./command_ban.js").execute()  ) vsExecuted = true;
-            else if (require("./command_unban.js").execute()  ) vsExecuted = true;
-            else if (require("./finaly.js").execute()   ) vsExecuted = true;
-            
-        });
-    });
+    if (require("./error_userBan.js").execute() ) vsExecuted = true; 
+    else if (require("./error_command.js").execute()  ) vsExecuted = true;
+    else if (require("./error_noPfix.js").execute() ) vsExecuted = true;
+    else if (require("./command_nya.js").execute()  ) vsExecuted = true;
+    else if (require("./command_purge.js").execute()    ) vsExecuted = true;
+    else if (require("./command_ban.js").execute()  ) vsExecuted = true;
+    else if (require("./command_unban.js").execute()  ) vsExecuted = true;
+    else if (require("./finaly.js").execute()   ) vsExecuted = true;
     
     return vsExecuted;
 }
