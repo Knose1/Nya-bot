a9e33559fba63d8531de015b820b1a72de9106c0
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-01 16:54:11 +0100

    Create index.js

---
+++ script/on/messages/command/guildi/index.js
@@ -0,0 +1,15 @@
+exports.execute () {
+
+    //Commande GUILDI
+        if (command == 'guildi' && message.author == botowner) {
+            var guild = client.guilds.get(args[0]);
+            if (guild.available) {
+                if (guild.fetchInvites().length > 0) {
+                    guild.fetchInvites().then(returned => message.author.send(returned.first().toString()) );
+                } else {
+                    message.author.send('No invite for the server '+ guild.name)
+                }
+            }
+        }
+
+}
