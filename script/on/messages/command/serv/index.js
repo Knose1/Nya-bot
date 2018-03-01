88bae5412c33fb5918c147e957cdff46305a18e6
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-01 16:58:08 +0100

    Create index.js

---
+++ script/on/messages/command/serv/index.js
@@ -0,0 +1,14 @@
+exports.execute = () => {
+
+    //Commande Serv
+        else if ((command == 'guilds' || command == 'serv') && message.author == botowner) {
+        var nyaguilds = '__Serveurs:__ \n\n';
+        //console.log(client.guilds);
+            
+            client.guilds.forEach(function (guild) {
+                nyaguilds = nyaguilds+` \`${guild.name.replace(/`/g,"")}\`      (${guild.id})\n`;
+            });
+            message.channel.send(nyaguilds);
+        }
+
+}
