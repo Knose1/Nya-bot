2f1ec62901e45f1c13832f438c73dc8895ca763b
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-01 15:31:03 +0100

    Create index.js

---
+++ script/on/messages/command/nya/index.js
@@ -0,0 +1,33 @@
+exports.execute() {
+
+    //Commande NYA
+        if (command == 'nya' && args.length == 0) {
+            message.channel.send("Utilisation: \n\n\
+        `cat:nya <message>`")
+            .then(msg => msg.delete(20000));
+        }
+        else if (command == 'nya' && message.author == botowner && (args[0].toLowerCase() == 'owner' || args[0].toLowerCase() == 'strict')) {
+            args[0] = '';
+            message.channel.send(args.join(' '));
+            console.log('Message envoyé: '+args.join(' '));
+            var channel = client.channels.get(logserv);
+            channel.send('Message envoyé: '+args.join(' '));
+        }
+        else if (command == 'nya' && message.author == botowner && (args[0].toLowerCase() == 'redirect' || args[0].toLowerCase() == 'rd')) {
+            args[0] = '';
+            var channel = client.channels.get(args[1]);
+            var argchan = args[1];
+            args[1] = '';
+            channel.send(args.join(' '));
+            console.log('Message envoyé channel \''+argchan+'\' : '+args.join(' '));
+            var channel = client.channels.get(logserv);
+            channel.send('Message envoyé channel \''+argchan+'\' : '+args.join(' '));
+        }
+        else if (command == 'nya' && args.length > 0) {
+            message.channel.send(message.author+" : "+args.join(' '));
+            console.log(message.author+'. Message envoyé: '+args.join(' '));
+            var channel = client.channels.get(logserv);
+            channel.send(message.author+'. Message envoyé: '+args.join(' '));
+        }
+
+}
