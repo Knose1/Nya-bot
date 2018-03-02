308f2ce67c0c97962a86a9c20f1d2eab2a691025
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-02 01:15:38 +0100

    Create anime.js

---
+++ script/on/messages/command/anime/anime.js
@@ -0,0 +1,22 @@
+var listAnime1 = Array();
+listAnime1[0] = {
+
+  name: '',
+  image: '',
+  
+  story_fr: '', //Synopsis
+  story_en: '',
+  
+  anime_fr: '', //Exemple: Anime ultime
+  anime_en: '',
+  
+  op: '',
+  opFull: '',
+  ed: '',
+  edFull: ''
+
+};
+
+export.load = () => {
+  return listAnime = listAnime1;
+}
