19a2d0f7d2b348ad05952f36d274ed00c0b52214
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-02 15:27:16 +0100

    Update anime.js

--- script/on/messages/command/anime/anime.js
+++ script/on/messages/command/anime/anime.js
@@ -1,20 +1,47 @@
 var listAnime1 = Array();
+
+/*  {
+
+    name: '',
+    image: '',
+    
+    story_fr: ``, //Synopsis
+    story_en: '',
+    
+    anime_fr: '', //Exemple: Anime ultime
+    anime_en: '',
+    
+    op: '',
+    opFull: '',
+    ed: '',
+    edFull: '',
+  
+    working: true, //Manque t'il des informations ?
+
+    }
+*/
+
 listAnime1[0] = {
 
-  name: '',
-  image: '',
+    name: '07 Ghost',
+    image: 'https://www.rightstufanime.com/images/productImages/875707079099_anime-07-Ghost-DVD-primary.jpg?resizeid=3&resizeh=600&resizew=600',
   
-  story_fr: '', //Synopsis
-  story_en: '',
+    story_fr: `L'académie militaire élitiste de Barsburg forme et entraine les futurs soldats de la nation. Tout n'est pas rose pour Teito Klein, ancien esclave et orphelin. Mais il parvient à faire face à sa condition et au regard de ses pairs, et à se distinguer comme étant l'un des meilleurs de sa promotion, lui et son ami Mikage.
+Mais l'examen final de l'académie se révèle bien au dessus de leurs craintes, car ils doivent éliminer un imposant prisonnier de guerre qui n'a aucun mal à faire mordre la poussière à ses camarades. Malgré cela, les deux amis parviennent à vaincre l'adversaire en usant de leur magie, le Zaiphon.
+Mais la rencontre avec le directeur de l'académie, insatisfait du dénouement de l'examen, va complètement chambouler Teito : devant lui se tient l'assassin de son père...\n
+((Source: Anime ultime)[http://www.anime-ultime.net/file-0-1/1719/07-Ghost-vostfr])`, //Synopsis
+    
+    story_en: '',
   
-  anime_fr: '', //Exemple: Anime ultime
-  anime_en: '',
+    anime_fr: 'http://www.anime-ultime.net/file-0-1/1719/07-Ghost-vostfr', //Exemple: Anime ultime
+    anime_en: '',
   
-  op: '',
-  opFull: '',
-  ed: '',
-  edFull: ''
+    op: 'https://www.youtube.com/watch?v=loeU-MZSX0k',
+    opFull: 'https://www.youtube.com/watch?v=OINW8ObizhY',
+    ed: 'https://www.youtube.com/watch?v=x8fhSxxvFn4',
+    edFull: 'https://www.youtube.com/watch?v=pz4jxQWxC08',
 
+    working: true,
 };
 
 exports.load = () => {
