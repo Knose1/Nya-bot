d3b1c821c62669b729dab50bc0699ece3d95c37e
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-03 15:49:45 +0100

    Update anime.js

--- script/on/messages/command/anime/anime.js
+++ script/on/messages/command/anime/anime.js
@@ -29,7 +29,7 @@ listAnime1[0] = {
     story_fr: `L'académie militaire élitiste de Barsburg forme et entraine les futurs soldats de la nation. Tout n'est pas rose pour Teito Klein, ancien esclave et orphelin. Mais il parvient à faire face à sa condition et au regard de ses pairs, et à se distinguer comme étant l'un des meilleurs de sa promotion, lui et son ami Mikage.
 Mais l'examen final de l'académie se révèle bien au dessus de leurs craintes, car ils doivent éliminer un imposant prisonnier de guerre qui n'a aucun mal à faire mordre la poussière à ses camarades. Malgré cela, les deux amis parviennent à vaincre l'adversaire en usant de leur magie, le Zaiphon.
 Mais la rencontre avec le directeur de l'académie, insatisfait du dénouement de l'examen, va complètement chambouler Teito : devant lui se tient l'assassin de son père...\n
-((Source: Anime ultime)[http://www.anime-ultime.net/file-0-1/1719/07-Ghost-vostfr])`, //Synopsis
+([Source: Anime ultime](http://www.anime-ultime.net/file-0-1/1719/07-Ghost-vostfr))`, //Synopsis
     
     story_en: 'undefined',
   
