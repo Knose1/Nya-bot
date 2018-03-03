5a4328d71ce64f01685e1ee38187fa3410a2eb8b
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-03 17:11:36 +0100

    Update index.js

--- script/on/messages/command/anime/index.js
+++ script/on/messages/command/anime/index.js
@@ -13,7 +13,7 @@ exports.execute = () => {
             .addBlankField()
             .addField("▬▬▬▬▬▬▬▬▬▬▬▬▬▬",`[Opening -> ${anime.op}](${anime.op})\n[Opening Full -> ${anime.opFull}](${anime.opFull})`, true)
             .addField("▬▬▬▬▬▬▬▬▬▬▬▬▬▬",`[Ending -> ${anime.ed}](${anime.ed})\n[Opening Full -> ${anime.edFull}](${anime.edFull})`, true)
-            .addField("▬▬▬▬▬▬▬▬▬▬▬▬▬▬", "__Voir les épisodes:__\n\n\n" + anime.anime_fr, false)
+            .addField("▬▬▬▬▬▬▬▬▬▬▬▬▬▬", "__Voir les épisodes:__\n\n" + anime.anime_fr, false)
             .setFooter(message.author.tag, message.author.avatarURL);
         
         var embeden = new Discord.RichEmbed()
@@ -24,7 +24,7 @@ exports.execute = () => {
             .addBlankField()
             .addField("▬▬▬▬▬▬▬▬▬▬▬▬▬▬",`[Opening -> ${anime.op}](${anime.op})\n[Opening Full -> ${anime.opFull}](${anime.opFull})`, true)
             .addField("▬▬▬▬▬▬▬▬▬▬▬▬▬▬",`[Ending -> ${anime.ed}](${anime.ed})\n[Opening Full -> ${anime.edFull}](${anime.edFull})`, true)
-            .addField("▬▬▬▬▬▬▬▬▬▬▬▬▬▬", "__See the episodes:__\n\n\n" + anime.anime_en, false)
+            .addField("▬▬▬▬▬▬▬▬▬▬▬▬▬▬", "__See the episodes:__\n\n" + anime.anime_en, false)
             .setFooter(message.author.tag, message.author.avatarURL);
         
         message.channel.send(embedfr)
