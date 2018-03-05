d6eeb6377d4adee61b61988cc96ad3a612e80be6
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-05 20:52:56 +0100

    Update index.js

--- script/on/messages/command/devon/index.js
+++ script/on/messages/command/devon/index.js
@@ -1,7 +1,9 @@
 exports.execute = () => {
 
     if (message.author == botowner && BotOnDev) {
-    
+        
+        client.user.setStatus('online');
+        client.user.setActivity(`cat:help | Nya!Bot est en marche, avec ${client.users.size} users, dans ${client.channels.size} salons et ${client.guilds.size} serveurs.`,{type: "PLAYING"});
         BotOnDev = false;
         
     } else if (message.author == botowner && !BotOnDev) {
@@ -10,7 +12,6 @@ exports.execute = () => {
         client.user.setStatus('idle');
         client.user.setActivity(`Developping . . .`,{type: "PLAYING"});
         noGame = 'activé';
-        return;
     
     }
 }
