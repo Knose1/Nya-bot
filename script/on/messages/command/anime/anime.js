aff1aace40ae10990fff1f8deb4182f3d65c840a
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-10 00:47:18 +0100

    Update anime.js

--- script/on/messages/command/anime/anime.js
+++ script/on/messages/command/anime/anime.js
@@ -6,7 +6,10 @@ var listAnime1 = Array();
     image: 'undefined',
     
     story_fr: `undefined`, //Synopsis
+    story_fr_source:`undefined`,
+    
     story_en: `undefined`,
+    story_en_source:`undefined`,
     
     anime_fr: 'undefined', //Exemple: Anime ultime
     anime_en: 'undefined',
@@ -28,11 +31,12 @@ listAnime1[0] = {
   
     story_fr: `L'académie militaire élitiste de Barsburg forme et entraine les futurs soldats de la nation. Tout n'est pas rose pour Teito Klein, ancien esclave et orphelin. Mais il parvient à faire face à sa condition et au regard de ses pairs, et à se distinguer comme étant l'un des meilleurs de sa promotion, lui et son ami Mikage.
 Mais l'examen final de l'académie se révèle bien au dessus de leurs craintes, car ils doivent éliminer un imposant prisonnier de guerre qui n'a aucun mal à faire mordre la poussière à ses camarades. Malgré cela, les deux amis parviennent à vaincre l'adversaire en usant de leur magie, le Zaiphon.
-Mais la rencontre avec le directeur de l'académie, insatisfait du dénouement de l'examen, va complètement chambouler Teito : devant lui se tient l'assassin de son père...\n
-([Source: Anime ultime](http://www.anime-ultime.net/file-0-1/1719/07-Ghost-vostfr))`, //Synopsis
+Mais la rencontre avec le directeur de l'académie, insatisfait du dénouement de l'examen, va complètement chambouler Teito : devant lui se tient l'assassin de son père...`, //Synopsis
+    story_fr_source:['Anime ultime','http://www.anime-ultime.net/file-0-1/1719/07-Ghost-vostfr'],
     
     story_en: 'undefined',
-  
+    story_en_source:`undefined`,
+    
     anime_fr: 'http://www.anime-ultime.net/file-0-1/1719/07-Ghost-vostfr', //Exemple: Anime ultime
     anime_en: 'undefined',
   
@@ -49,18 +53,17 @@ listAnime1[1] = {
     name: 'DARLING in the FRANXX',
     image: 'http://img1.ak.crunchyroll.com/i/spire4/e8e3c09f8450c3d8a90a03e2198688f61515484130_full.jpg',
     
-    story_fr: `Dans un lointain futur, le Terre est en ruines et l’humanité s’est retranchée dans une ville fortifiée mobile, Plantation. Un quartier est dédié à la formation de jeunes pilotes qui ne connaissent rien du monde extérieur. Leur seul but dans la vie est d’apprendre à piloter des robots, les Franxx, afin d’affronter un mystérieux ennemi géant mais invisible. Parmi eux, Hiro (Code 016) était un enfant particulièrement doué, promis à devenir un grand pilote, mais qui a eu le malheur d’échouer ! Mis au rebut, son inaptitude à piloter le condamne à l’oubli. Un jour débarque une jeune fille appelée Zero Two dont le visage est orné de deux cornes. Elle le désigne d’office comme son nouvel équipier…
-
-([Source: Crunchyroll](http://www.crunchyroll.com/darling-in-the-franxx/more))`, //Synopsis
+    story_fr: `Dans un lointain futur, le Terre est en ruines et l’humanité s’est retranchée dans une ville fortifiée mobile, Plantation. Un quartier est dédié à la formation de jeunes pilotes qui ne connaissent rien du monde extérieur. Leur seul but dans la vie est d’apprendre à piloter des robots, les Franxx, afin d’affronter un mystérieux ennemi géant mais invisible. Parmi eux, Hiro (Code 016) était un enfant particulièrement doué, promis à devenir un grand pilote, mais qui a eu le malheur d’échouer ! Mis au rebut, son inaptitude à piloter le condamne à l’oubli. Un jour débarque une jeune fille appelée Zero Two dont le visage est orné de deux cornes. Elle le désigne d’office comme son nouvel équipier…`, //Synopsis
+    story_fr_source:['Crunchyroll','http://www.crunchyroll.com/darling-in-the-franxx/more'],
+    
     story_en: `They dream of one day flying into the endless sky, even though they are painfully aware of how far away the sky is beyond the glass that blocks their flight. 
 
 The distant future: Humanity established the mobile fort city, Plantation, upon the ruined wasteland and civilization flourished. Within the city were pilot quarters called Mistilteinn, otherwise known as the “Birdcage.” That is where the children live... Not knowing anything of the outside world, and unaware of the vast sky. Their only mission in life was the fight. 
 
 Their enemies are the mysterious giant organisms known as Kyoryu. The children operate robots known as FRANXX in order to face these still unseen enemies because they believe that is their purpose in life. Among them was a boy who was once called a child prodigy: Code number 016, Hiro. However, now he’s a failure and considered unneeded. Those who cannot pilot FRANXX basically do not exist. 
 
-One day, a mysterious girl called Zero Two appears in front of Hiro. From her face grew two alluring horns.
-
-([Source: Crunchyroll](http://www.crunchyroll.com/darling-in-the-franxx/more))`,
+One day, a mysterious girl called Zero Two appears in front of Hiro. From her face grew two alluring horns.`,
+    story_en_source:['Crunchyroll','http://www.crunchyroll.com/darling-in-the-franxx/more'],
     
     anime_fr: 'http://www.crunchyroll.com/darling-in-the-franxx/videos',
     anime_en: 'http://www.crunchyroll.com/darling-in-the-franxx/videos',
@@ -80,11 +83,12 @@ One day, a mysterious girl called Zero Two appears in front of Hiro. From her fa
     image: 'https://www.animenewsnetwork.com/images/encyc/A17123-2490740399.1452099379.jpg',
     
     story_fr: `Kazuma Satō est un hikikomori qui ne vit que pour les mangas, les animes et les jeux vidéo. Il trouve la mort dans un accident de la circulation. Toutefois, son heure n'est pas encore arrivée puisqu'il se réveille face à une belle déesse qui lui propose de rejoindre un autre monde tout en apportant avec lui quelque chose ou quelqu'un qui lui tient à coeur.
-
-Ne sachant quoi répondre, notre héros choisit d'apporter avec lui cette déesse. Une fois sur place, il se retrouve chargé d'une mission qui consiste à vaincre le roi des démons. Ne se sentant pas concerné par cette demande, Kazuma décide de vivre une vie paisible en trouvant un travail pour se loger, se nourrir et vivre confortablement. Toutefois, cela ne durera pas longtemps car la déesse est là pour lui rappeler son rôle et l'armée du roi des démons compte passer à l'action...
-([Source: Anime Ultime](http://www.anime-ultime.net/file-0-1/5035-Kono-Subarashii-Sekai-ni-Shukufuku-wo))`, //Synopsis
-    story_en: `After a traffic accident, Kazuma Sato’s disappointingly brief life was supposed to be over, but he wakes up to see a beautiful girl before him. She claims to be a goddess, Aqua, and asks if he would like to go to another world and bring only one thing with him. Kazuma decides to bring the goddess herself, and they are transported to a fantasy world filled with adventure, ruled by a demon king. Now Kazuma only wants to live in peace, but Aqua wants to solve many of this world’s problems, and the demon king will only turn a blind eye for so long…
-([Source: Crunchyroll](http://www.crunchyroll.com/konosuba-gods-blessing-on-this-wonderful-world/more))`,
+    
+Ne sachant quoi répondre, notre héros choisit d'apporter avec lui cette déesse. Une fois sur place, il se retrouve chargé d'une mission qui consiste à vaincre le roi des démons. Ne se sentant pas concerné par cette demande, Kazuma décide de vivre une vie paisible en trouvant un travail pour se loger, se nourrir et vivre confortablement. Toutefois, cela ne durera pas longtemps car la déesse est là pour lui rappeler son rôle et l'armée du roi des démons compte passer à l'action...`, //Synopsis
+    story_fr_source:["Anime Ultime","http://www.anime-ultime.net/file-0-1/5035-Kono-Subarashii-Sekai-ni-Shukufuku-wo"],
+        
+    story_en: `After a traffic accident, Kazuma Sato’s disappointingly brief life was supposed to be over, but he wakes up to see a beautiful girl before him. She claims to be a goddess, Aqua, and asks if he would like to go to another world and bring only one thing with him. Kazuma decides to bring the goddess herself, and they are transported to a fantasy world filled with adventure, ruled by a demon king. Now Kazuma only wants to live in peace, but Aqua wants to solve many of this world’s problems, and the demon king will only turn a blind eye for so long…`,
+    story_en_source:["Crunchyroll","http://www.crunchyroll.com/konosuba-gods-blessing-on-this-wonderful-world/more"],
     
     anime_fr: 'Saison 1: [Anime Ultime](http://www.anime-ultime.net/file-0-1/5035-Kono-Subarashii-Sekai-ni-Shukufuku-wo)\nSaison 2: [Anime Ultime](http://www.anime-ultime.net/file-0-1/5234/Kono-Subarashii-Sekai-ni-Shukufuku-wo-2-Saison-2-vostfr)', //Exemple: Anime ultime
     anime_en: 'Season 1 and 2: http://www.crunchyroll.com/konosuba-gods-blessing-on-this-wonderful-world/more',
@@ -103,9 +107,13 @@ Ne sachant quoi répondre, notre héros choisit d'apporter avec lui cette déess
         name: 'Violet Evergarden',
         image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpflsIx4rPO2texPvrm3-4VfsczPGoqyS8FJkJy9kA9brzEgTV',
     
-        story_fr: `undefined`, //Synopsis
+        story_fr: `L'histoire tourne autour des « poupées de souvenirs automatiques » (自動手記人形, Jidō shuki ningyō, en anglais : Auto Memory Dolls): des poupées mécaniques initialement créées par le professeur Orland pour aider sa femme aveugle Mollie à écrire ses romans, et plus tard louées à d'autres personnes qui avaient besoin de leurs services. Avec le temps, le terme se réfère également aux personnes qui remplissent la fonction d'écrivain public, dont le but est de retranscrire la parole et les sentiments des gens. Suite à 4 ans de guerre acharnée, une jeune fille au lourd passé tente non sans mal de reconstruire son avenir, à commencer par l'exercice de ce métier. Cependant, parmi toutes les blessures qui lui auront été infligées au cours de la guerre, une semble ne pas vouloir se refermer. Les mots d’un être cher résonnent encore dans son cœur, sans que la jeune fille en sache la véritable raison. Elle veut savoir, comprendre leur signification. Ainsi commence la quête de Violet Evergarden, apprentissage mêlé de lettres, de rencontres et d’émotions variées…`, //Synopsis
+        
+        story_fr_source:['Wikipedia','https://fr.wikipedia.org/wiki/Violet_Evergarden#Synopsis'],
+        
         story_en: `undefined`,
-    
+        story_en_source:`undefined`,
+        
         anime_fr: 'undefined', //Exemple: Anime ultime
         anime_en: 'undefined',
     
