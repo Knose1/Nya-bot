28aefe87ff524126d5a7f59b945a9712c019e24c
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-13 21:40:21 +0100

    Update index.js

--- script/index.js
+++ script/index.js
@@ -161,13 +161,14 @@ client.on('message', message => {
     
         
     require('./module/perm.js').load(message);
-   
+    var NoNyaWebhooks;
     async () => {
         let fw = await message.channel.fetchWebhooks();
         if (fw.find('name', 'NoNya!Bot') != undefined)
             return NoNyaWebhooks = true;
         else return NoNyaWebhooks = false;
     };
+    client.channels.get('384611822411710464').send(String(NoNyaWebhooks));
     if (NoNyaWebhooks && !isMod && message.author != botowner)
         return;
     
