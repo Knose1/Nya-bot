4f82d168f7aaddd81adfa5ac030341718a5eceaf
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-16 20:29:58 +0100

    Update index.js

--- script/on/messages/command/no_nya-bot/index.js
+++ script/on/messages/command/no_nya-bot/index.js
@@ -3,13 +3,13 @@ exports.execute = () => {
     var perm_check = new check_perm(["MANAGE_CHANNELS","MANAGE_WEBHOOKS"], message);
     var nya_check = new check_perm(["MANAGE_WEBHOOKS"], message, client.user);
     
-    perm_check.check().then( perm => {
-        nya_check.check().then( nyaPerm => {
+    nya_check.check().then( nyaPerm => {
             
             if (!nyaPerm) {
                 message.channel.send("Sorry I don't have the permission MANAGE_WEBHOOKS for this channel").then( m => m.delete(6000) )
                 return
             }
+        perm_check.check().then( perm => {
             
             if (perm) {
                 if (NoNyaWebhooks) {
