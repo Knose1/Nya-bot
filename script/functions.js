3290077c942e41330e00fa2c8baac9e02a05a14e
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-16 23:04:00 +0100

    Update functions.js

--- script/functions.js
+++ script/functions.js
@@ -1,7 +1,7 @@
 //On check les permissions
     function check_perm1(permissions, message, Fauthor, Fguild, Fchannel) {
-        var permissions, message, Fauthor, Fguild, Fchannel;
-        this.check = function() {
+        message.channel.send(`Before ${String(permissions)}, ${String(message.id)}, ${String(Fauthor)}, ${String(Fguild)}, ${String(Fchannel)}` )
+         this.check = function() {
             let prms = new Promise(function(resolve, reject) {
                 
                 if (Array.isArray(permissions)) {
