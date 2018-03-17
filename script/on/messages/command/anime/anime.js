b08dc786a1a897023b442adab8f2efa5ad2acc86
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-17 14:55:45 +0100

    Update anime.js

--- script/on/messages/command/anime/anime.js
+++ script/on/messages/command/anime/anime.js
@@ -1,6 +1,100 @@
-var listAnime1 = Array();
+function animelist() {
 
-/* listAnime1[i] = {
+    this.add = function(anime) {
+        
+        if (typeof(anime) == "object" && !Array.isArray(anime)) {
+            this.animes.push(anime);
+        } else throw new Error("anime is not an [object Object] at animelist.add(anime)");
+        
+    }
+    this.animes = [];
+}
+
+function anime(arg1) {
+    
+    if (typeof(arg1) == "object" && !Array.isArray(arg1)) this.object = arg1;
+    
+    else if (typeof(arg1) != "string" && arg1 != undefined) throw new Error("name is not a string at anime(name)");
+    
+    else this.object = {name:arg1};
+    /*name*/
+    this.setname = function(name) {
+        if (typeof(name) != "string") throw new Error("name is not a string at setname(name)")
+        this.object.name = name;
+    }
+    
+    /*image*/
+    this.setimage = function(image) {
+        if (typeof(image) != "string") throw new Error("image is not a string at setimage(image)")
+        this.object.image = image;
+    }
+    
+    /*story_fr*/
+    this.setstory_fr = function(story_fr) {
+        if (typeof(story_fr) != "string") throw new Error("story_fr is not a string at setstory_fr(story_fr)")
+        this.object.story_fr = story_fr;
+    }
+    
+    /*story_fr_source*/
+    this.setstory_fr_source = function(story_fr_source) {
+        if ( !Array.isArray(story_fr_source) && story_fr_source != "undefined") throw new Error("story_fr_source is not an array at setstory_fr_source(story_fr_source)")
+        this.object.story_fr_source = story_fr_source;
+    }
+    
+    /*story_en*/
+    this.setstory_en = function(story_en) {
+        if (typeof(story_en) != "string") throw new Error("story_en is not a string at setstory_fr(story_en)")
+        this.object.story_en = story_en;
+    }
+    
+    /*story_en_source*/
+    this.setstory_en_source = function(story_en_source) {
+        if ( !Array.isArray(story_en_source) && story_en_source != "undefined") throw new Error("story_en_source is not an array at setstory_en_source(story_en_source)")
+        this.object.story_en_source = story_en_source;
+    }
+    
+    /*anime_fr*/
+    this.setanime_fr = function(anime_fr) {
+        if (typeof(anime_fr) != "string") throw new Error("anime_fr is not a string at setanime_fr(anime_fr)")
+        this.object.anime_fr = anime_fr;
+    }
+    
+    /*anime_en*/
+    this.setanime_en = function(anime_en) {
+        if (typeof(anime_en) != "string") throw new Error("anime_en is not a string at setanime_en(anime_en)")
+        this.object.anime_en = anime_en;
+    }
+    
+    /*op*/
+    this.setop = function(op) {
+        if ( !Array.isArray(op)) throw new Error("op is not an array at setop(op)")
+        this.object.op = op;
+    }
+    
+    /*opFull*/
+    this.setopFull = function(opFull) {
+        if ( !Array.isArray(opFull)) throw new Error("opFull is not an array at setopFull(opFull)")
+        this.object.opFull = opFull;
+    }
+    
+    /*ed*/
+    this.seted = function(ed) {
+        if ( !Array.isArray(ed)) throw new Error("ed is not an array at seted(ed)")
+        this.object.ed = ed;
+    }
+    
+    /*edFull*/
+    this.setedFull = function(edFull) {
+        if ( !Array.isArray(edFull)) throw new Error("edFull is not an array at setedFull(edFull)")
+        this.object.edFull = edFull;
+    }
+    
+    this.addTo = function(NameOfvar) {
+        NameOfvar.add(this.object);
+    }
+}
+
+/* listAnime1[listAnime1.length] = {
 
     name: 'undefined',
     image: 'undefined',
@@ -23,8 +117,9 @@ var listAnime1 = Array();
 
     };
 */
+var listAnime1 = []; //new animelist();
 
-listAnime1[0] = {
+listAnime1[listAnime1.length] = {
 
     name: '07 Ghost',
     image: 'https://www.rightstufanime.com/images/productImages/875707079099_anime-07-Ghost-DVD-primary.jpg?resizeid=3&resizeh=600&resizew=600',
@@ -48,7 +143,7 @@ Mais la rencontre avec le directeur de l'académie, insatisfait du dénouement d
     working: true,
 };
 
-listAnime1[1] = {
+listAnime1[listAnime1.length] = {
 
     name: 'DARLING in the FRANXX',
     image: 'http://img1.ak.crunchyroll.com/i/spire4/e8e3c09f8450c3d8a90a03e2198688f61515484130_full.jpg',
@@ -69,7 +164,7 @@ One day, a mysterious girl called Zero Two appears in front of Hiro. From her fa
     anime_en: 'http://www.crunchyroll.com/darling-in-the-franxx/videos',
     
     op: ['https://youtu.be/3x0NEd9t1yQ'],
-    opFull: ['https://youtu.be/iTgPpXDm8v0'],
+    opFull: ['https://youtu.be/mzJqxT1UGho'],
     ed: ['https://youtu.be/h8RzhkWr0WY'],
     edFull: ['https://youtu.be/rV8MLwYtX9M'],
   
@@ -77,7 +172,7 @@ One day, a mysterious girl called Zero Two appears in front of Hiro. From her fa
 
     };
 
-    listAnime1[2] = {
+    listAnime1[listAnime1.length] = {
 
     name: 'Konosuba (Kono Subarashii Sekai ni Shukufuku wo!)',
     image: 'https://www.animenewsnetwork.com/images/encyc/A17123-2490740399.1452099379.jpg',
@@ -125,6 +220,29 @@ Ne sachant quoi répondre, notre héros choisit d'apporter avec lui cette déess
         working: true, //Manque t'il des informations ?
 
         };
+
+        listAnime1[listAnime1.length] = {
+
+    name: 'Fate/EXTRA Last Encore',
+    image: 'undefined',
+    
+    story_fr: `undefined`, //Synopsis
+    story_fr_source:`undefined`,
+    
+    story_en: `undefined`,
+    story_en_source:`undefined`,
+    
+    anime_fr: 'undefined', //Exemple: Anime ultime
+    anime_en: 'undefined',
+    
+    op: ['undefined'],
+    opFull: ['https://www.youtube.com/watch?v=pq2Ira7hApg'],
+    ed: ['undefined'],
+    edFull: ['https://www.youtube.com/watch?v=Ok-eSJNChss'],
+  
+    working: true, //Manque t'il des informations ?
+
+    };
 
 exports.load = () => {
   return listAnime = listAnime1;
