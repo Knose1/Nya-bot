dc94f155fb30f8096abd70316d08a19087f063d0
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-17 09:23:47 +0100

    Update index.js

--- script/index.js
+++ script/index.js
@@ -176,171 +176,178 @@ client.on('message', message => {
         } catch (err) {
             NoNyaWebhooks = false;
         }
-    if (NoNyaWebhooks && !isMod && message.author != botowner)
-        return;
+    var options_pch2 = {
+        permissions: ["MANAGE_CHANNELS","MANAGE_WEBHOOKS"],
+        message: message,
+    }
+    var perm_checking = new check_perm(options_pch);
+    perm_checking.then(pprm => {
+        if (NoNyaWebhooks && !pprm)
+            return;
     
-    //On récupère le suffix du vs
-    var isVs = false;
-    if (message.channel.name.indexOf('nya-bot-vs') == 0) {
-        var Pfx = message.channel.name.slice('nya-bot-vs'.length);
-        if (Pfx == undefined) Pfx = '';
-        if (Pfx.indexOf('-') == 0) Pfx = Pfx.slice('-'.length);
-        if (Pfx == undefined) Pfx = '';
+        //On récupère le suffix du vs
+        var isVs = false;
+        if (message.channel.name.indexOf('nya-bot-vs') == 0) {
+            var Pfx = message.channel.name.slice('nya-bot-vs'.length);
+            if (Pfx == undefined) Pfx = '';
+            if (Pfx.indexOf('-') == 0) Pfx = Pfx.slice('-'.length);
+            if (Pfx == undefined) Pfx = '';
     
-        var isPfx = false
-        VsPrefixs.forEach( p => {
-            if (Pfx == p) isPfx = true;
+            var isPfx = false
+            VsPrefixs.forEach( p => {
+                if (Pfx == p) isPfx = true;
+            });
+        
+            if (isPfx) 
+                isVs = true;
+        
+            //console.log(isVs+" ; "+Pfx);
+        }
+    
+        //On récupère la liste des ban
+        var guild = client.guilds.get('406926403628695556');
+        var vsban = new Array();
+        var vsx = -1;
+        guild.roles.forEach(function (role) {
+            vsx = vsx+1;
+            vsban[vsx] = role.name;
+            //console.log(role.name);
         });
+        //console.log(message.author.id);
     
-        if (isPfx) 
-            isVs = true;
-    
-        //console.log(isVs+" ; "+Pfx);
-    }
-    
-    //On récupère la liste des ban
-    var guild = client.guilds.get('406926403628695556');
-    var vsban = new Array();
-    var vsx = -1;
-    guild.roles.forEach(function (role) {
-        vsx = vsx+1;
-        vsban[vsx] = role.name;
-        //console.log(role.name);
-    });
-    //console.log(message.author.id);
-    
-    //On regarde si la personne est ban
-    var isbanned = false;
-    vsban.forEach(function (banned) {
-        if (message.author.id == banned && message.author != botowner && message.author.id != mention) {
-            isbanned = true;
-        }
-    });
-    
-    /*DEBUT BOT*/
-    //ignorer si c'est un bot (sauf s'il parle dans le vs sous certaines conditions
-    if(message.author.bot == true) {
-
-                    //Bot ban et bot différent de nya!bot
-                    if (isVs && isbanned == true && message.author.id != mention) {
-                        message.delete(500)
-                            .then(msg => console.log(`Message supprimé, raison: commande; Auteur: ${msg.author}`))
-                            .catch(console.error);
-                        return;
-                    }
-                    //Nya!bot commande
-                    else if ((isVs || (message.guild.id == "377892426569744387" && message.channel.name == "nya-bot-vs-log")) && iscommand == true) {
-                        message.delete(500)
-                            .then(msg => console.log(`Message supprimé, raison: commande; Auteur: ${msg.author}`))
-                            .catch(console.error);
-                        return;
-                    }
-                    //Si pas de -- et pas de // et différent de nya!bot
-                    else if ((isVs || (message.guild.id == "377892426569744387" && message.channel.name == "nya-bot-vs-log")) && (message.content.indexOf('--') != 0 && message.content.indexOf('//') != 0) && message.author.id != mention) {
-                        message.delete(500)
-                            .then(msg => console.log(`Message supprimé, raison: commande; Auteur: ${msg.author}`))
-                            .catch(console.error);
-                        return;
-                    }
-                    else if((isVs || (message.guild.id == "377892426569744387" && message.channel.name == "nya-bot-vs-log")) && (message.content.indexOf('--') == 0 || message.content.indexOf('//') == 0)) {
-                    } else return;
-        
-    }
-    /*Fin BOT*/
-    
-    //si c'est une commande, récupérer les arguments, la commande et supprimer le message
-        if (message.content.indexOf(prefix) == 0) {
-            var iscommand = true;
-            var args = message.content.slice(prefix.length).trim().replace(/\n/g," \n").split(/ +/g);
-            var command = args.shift().toLowerCase();
-    
-            message.delete(500)
-                .then(msg => console.log(`Message supprimé, raison: commande; Auteur: ${msg.author}`))
-                .catch(console.error);
-        }
-    
-    /*if (message.author == botowner && (!isVs || (message.guild.id == "377892426569744387" && message.channel.name != "nya-bot-vs-log"))) {
-        if (Database('407142766674575361',['user:','xp:']).get('user:',message.author.id,['xp:'])['xp:'].value[0] != 'NaN') {
-            Database('407142766674575361',['user:','xp:']).get('user:',message.author.id,['xp:'])['xp:'].set([String(Number(Database('407142766674575361',['user:','xp:']).get('user:',message.author.id,['xp:'])['xp:'].value[0] + 1))]);
-        
-            if( Math.floor( (Number(Database('407142766674575361',['user:','xp:']).get('user:',message.author.id,['xp:'])['xp:'].value[0] ) +1) / 10 ) == (Number(Database('407142766674575361',['user:','xp:']).get('user:',message.author.id,['xp:'])['xp:'].value[0] ) +1)/ 10) {
-                message.channel.send(`Bravo Knose1 tu as ${Number(Database('407142766674575361',['user:','xp:']).get('user:',message.author.id,['xp:'])['xp:'].value[0]) + 1} xp`)
-                    .then(msg => msg.delete(10000));
+        //On regarde si la personne est ban
+        var isbanned = false;
+        vsban.forEach(function (banned) {
+            if (message.author.id == banned && message.author != botowner && message.author.id != mention) {
+                isbanned = true;
             }
-        }
-    }*/
-    
-    //VS
-        if (require("./on/messages/vs/error_userBan.js").execute(message, isVs, isbanned) );
-        else if (require("./on/messages/vs/error_command.js").execute(message, isVs, iscommand)  );
-        else if (require("./on/messages/vs/error_noPfix.js").execute(message, isVs) );
-        else if (require("./on/messages/vs/command_nya.js").execute(message, isVs)  );
-        else if (require("./on/messages/vs/command_purge.js").execute(message, isVs, Pfx)    );
-        else if (require("./on/messages/vs/command_ban.js").execute(message, isVs, isbanned, vsban)  );
-        else if (require("./on/messages/vs/command_unban.js").execute(message, isVs, isbanned, vsban)  );
-        else if (require("./on/messages/vs/finaly.js").execute(message, isVs, Pfx)   );
-    
-    
-    //RPG
-    //else if (require("./on/messages/rpg/index.js").execute(message)    );
-    
-    //COMMAND
-    else if (iscommand == true) {
+        });
         
-        
-        if (command == "github") {
-            command = "git";
-        }
-        if (command == "guilds") {
-            command = "serv"
-        }
-        if (command == "cg") {
-            command = "channelget"
-        }
-        if (command == "emg") {
-            command = "emojiget"
-        }
-        if (command == "mt") {
-            command = "mathstest"
-        }
-        
-        //pas de commande -> ne rien faire
-        if (command == '') {
-        
-        return;
-        }
-        try {
-            var funcComm = String(require(`./on/messages/command/${command}/index.js`).execute);
-            var toEv = funcComm.slice(7, funcComm.length - 1)/*.replace(/\n/g,"").replace(/ +/g," ")*/;
-            //console.log(toEv);
-            eval(toEv);
-        }
-        catch (err) {
+        /*DEBUT BOT*/
+        //ignorer si c'est un bot (sauf s'il parle dans le vs sous certaines conditions
+        if(message.author.bot == true) {
+    
+                        //Bot ban et bot différent de nya!bot
+                        if (isVs && isbanned == true && message.author.id != mention) {
+                            message.delete(500)
+                                .then(msg => console.log(`Message supprimé, raison: commande; Auteur: ${msg.author}`))
+                                .catch(console.error);
+                            return;
+                        }
+                        //Nya!bot commande
+                        else if ((isVs || (message.guild.id == "377892426569744387" && message.channel.name == "nya-bot-vs-log")) && iscommand == true) {
+                            message.delete(500)
+                                .then(msg => console.log(`Message supprimé, raison: commande; Auteur: ${msg.author}`))
+                                .catch(console.error);
+                            return;
+                        }
+                        //Si pas de -- et pas de // et différent de nya!bot
+                        else if ((isVs || (message.guild.id == "377892426569744387" && message.channel.name == "nya-bot-vs-log")) && (message.content.indexOf('--') != 0 && message.content.indexOf('//') != 0) && message.author.id != mention) {
+                            message.delete(500)
+                                .then(msg => console.log(`Message supprimé, raison: commande; Auteur: ${msg.author}`))
+                                .catch(console.error);
+                            return;
+                        }
+                        else if((isVs || (message.guild.id == "377892426569744387" && message.channel.name == "nya-bot-vs-log")) && (message.content.indexOf('--') == 0 || message.content.indexOf('//') == 0)) {
+                        } else return;
             
-                if (String(err).toLowerCase().indexOf(`Cannot find module './on/messages/command/${command}/index.js'`.toLowerCase()) == -1) {
-                    message.reply("Une ERREUR est survenue");
-                    
-                    var cleanERR = fulllog( util.inspect( clean(err), 1500 ) );
-                    client.users.get("375378900802338818").send(`__Une ERREUR est survenue:__ \n Auteur: ${message.author}\n Longueur de la commande: ${message.content.length}\n Commande: ${message.content.slice(0,1000)}`);
-                    
-                    if (undefined != cleanERR[0])
-                        client.users.get("375378900802338818").send(cleanERR[0]);
-                    if (undefined != cleanERR[1])
-                        client.users.get("375378900802338818").send(cleanERR[1]);
-                    
-                    haderror = true;
-                    client.user.setStatus('dnd');
-                    client.user.setActivity(`ERROR`,{type: "PLAYING"});
-                    
-                } else {
-                    if (message.guild.id != '110373943822540800') {
-                        message.channel.send('"'+message.content+' "'+" n'est pas une commande")
-                            .then(msg => msg.delete(15000));
-                    }
-                }
         }
-        iscommand = false;
-    }
+        /*Fin BOT*/
+    
+        //si c'est une commande, récupérer les arguments, la commande et supprimer le message
+            if (message.content.indexOf(prefix) == 0) {
+                var iscommand = true;
+                var args = message.content.slice(prefix.length).trim().replace(/\n/g," \n").split(/ +/g);
+                var command = args.shift().toLowerCase();
+        
+                message.delete(500)
+                    .then(msg => console.log(`Message supprimé, raison: commande; Auteur: ${msg.author}`))
+                    .catch(console.error);
+            }
+    
+        /*if (message.author == botowner && (!isVs || (message.guild.id == "377892426569744387" && message.channel.name != "nya-bot-vs-log"))) {
+            if (Database('407142766674575361',['user:','xp:']).get('user:',message.author.id,['xp:'])['xp:'].value[0] != 'NaN') {
+                Database('407142766674575361',['user:','xp:']).get('user:',message.author.id,['xp:'])['xp:'].set([String(Number(Database('407142766674575361',['user:','xp:']).get('user:',message.author.id,['xp:'])['xp:'].value[0] + 1))]);
+        
+               if( Math.floor( (Number(Database('407142766674575361',['user:','xp:']).get('user:',message.author.id,['xp:'])['xp:'].value[0] ) +1) / 10 ) == (Number(Database('407142766674575361',['user:','xp:']).get('user:',message.author.id,['xp:'])['xp:'].value[0] ) +1)/ 10) {
+                    message.channel.send(`Bravo Knose1 tu as ${Number(Database('407142766674575361',['user:','xp:']).get('user:',message.author.id,['xp:'])['xp:'].value[0]) + 1} xp`)
+                        .then(msg => msg.delete(10000));
+                }
+            }
+        }*/
+    
+        //VS
+            if (require("./on/messages/vs/error_userBan.js").execute(message, isVs, isbanned) );
+            else if (require("./on/messages/vs/error_command.js").execute(message, isVs, iscommand)  );
+            else if (require("./on/messages/vs/error_noPfix.js").execute(message, isVs) );
+            else if (require("./on/messages/vs/command_nya.js").execute(message, isVs)  );
+            else if (require("./on/messages/vs/command_purge.js").execute(message, isVs, Pfx)    );
+            else if (require("./on/messages/vs/command_ban.js").execute(message, isVs, isbanned, vsban)  );
+            else if (require("./on/messages/vs/command_unban.js").execute(message, isVs, isbanned, vsban)  );
+            else if (require("./on/messages/vs/finaly.js").execute(message, isVs, Pfx)   );
+     
+        
+        //RPG
+        //else if (require("./on/messages/rpg/index.js").execute(message)    );
+        
+        //COMMAND
+        else if (iscommand == true) {
+            
+            
+           if (command == "github") {
+                command = "git";
+            }
+            if (command == "guilds") {
+                command = "serv"
+            }
+            if (command == "cg") {
+                command = "channelget"
+            }
+            if (command == "emg") {
+                command = "emojiget"
+            }
+            if (command == "mt") {
+                command = "mathstest"
+            }
+            
+            //pas de commande -> ne rien faire
+            if (command == '') {
+            
+            return;
+            }
+            try {
+                var funcComm = String(require(`./on/messages/command/${command}/index.js`).execute);
+                var toEv = funcComm.slice(7, funcComm.length - 1)/*.replace(/\n/g,"").replace(/ +/g," ")*/;
+                //console.log(toEv);
+                eval(toEv);
+            }
+            catch (err) {
+            
+                    if (String(err).toLowerCase().indexOf(`Cannot find module './on/messages/command/${command}/index.js'`.toLowerCase()) == -1) {
+                        message.reply("Une ERREUR est survenue");
+                    
+                        var cleanERR = fulllog( util.inspect( clean(err), 1500 ) );
+                        client.users.get("375378900802338818").send(`__Une ERREUR est survenue:__ \n Auteur: ${message.author}\n Longueur de la commande: ${message.content.length}\n Commande: ${message.content.slice(0,1000)}`);
+                    
+                        if (undefined != cleanERR[0])
+                            client.users.get("375378900802338818").send(cleanERR[0]);
+                        if (undefined != cleanERR[1])
+                            client.users.get("375378900802338818").send(cleanERR[1]);
+                    
+                        haderror = true;
+                        client.user.setStatus('dnd');
+                        client.user.setActivity(`ERROR`,{type: "PLAYING"});
+                    
+                    } else {
+                        if (message.guild.id != '110373943822540800') {
+                            message.channel.send('"'+message.content+' "'+" n'est pas une commande")
+                                .then(msg => msg.delete(15000));
+                        }
+                    }
+            }
+            iscommand = false;
+        }
+    })
     };
     fuNoNyaWebhooks();
 });
