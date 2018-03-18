4b28063cb51e17abb9cce851db2054889e7e3a4d
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-18 14:16:00 +0100

    Update functions.js

--- script/functions.js
+++ script/functions.js
@@ -4,6 +4,7 @@ function animelist1() {
         
         if (typeof(anime) == "object" && !Array.isArray(anime)) {
             this.animes.push(anime);
+            return this;
         } else throw new Error("anime is not an [object Object] at animelist.add(anime)");
         
     }
@@ -21,72 +22,84 @@ function anime1(arg1) {
     this.setname = function(name) {
         if (typeof(name) != "string") throw new Error("name is not a string at setname(name)")
         this.object.name = name;
+        return this;
     }
     
     /*image*/
     this.setimage = function(image) {
         if (typeof(image) != "string") throw new Error("image is not a string at setimage(image)")
         this.object.image = image;
+        return this;
     }
     
     /*story_fr*/
     this.setstory_fr = function(story_fr) {
         if (typeof(story_fr) != "string") throw new Error("story_fr is not a string at setstory_fr(story_fr)")
         this.object.story_fr = story_fr;
+        return this;
     }
     
     /*story_fr_source*/
     this.setstory_fr_source = function(story_fr_source) {
         if ( !Array.isArray(story_fr_source) && story_fr_source != "undefined") throw new Error("story_fr_source is not an array at setstory_fr_source(story_fr_source)")
         this.object.story_fr_source = story_fr_source;
+        return this;
     }
     
     /*story_en*/
     this.setstory_en = function(story_en) {
         if (typeof(story_en) != "string") throw new Error("story_en is not a string at setstory_fr(story_en)")
         this.object.story_en = story_en;
+        return this;
     }
     
     /*story_en_source*/
     this.setstory_en_source = function(story_en_source) {
         if ( !Array.isArray(story_en_source) && story_en_source != "undefined") throw new Error("story_en_source is not an array at setstory_en_source(story_en_source)")
         this.object.story_en_source = story_en_source;
+        return this;
     }
     
     /*anime_fr*/
     this.setanime_fr = function(anime_fr) {
         if (typeof(anime_fr) != "string") throw new Error("anime_fr is not a string at setanime_fr(anime_fr)")
         this.object.anime_fr = anime_fr;
+        return this;
     }
     
     /*anime_en*/
     this.setanime_en = function(anime_en) {
         if (typeof(anime_en) != "string") throw new Error("anime_en is not a string at setanime_en(anime_en)")
         this.object.anime_en = anime_en;
+        return this;
     }
     
     /*op*/
     this.setop = function(op) {
         if ( !Array.isArray(op)) throw new Error("op is not an array at setop(op)")
         this.object.op = op;
+        return this;
     }
     
     /*opFull*/
     this.setopFull = function(opFull) {
         if ( !Array.isArray(opFull)) throw new Error("opFull is not an array at setopFull(opFull)")
         this.object.opFull = opFull;
+        return this;
     }
     
     /*ed*/
     this.seted = function(ed) {
         if ( !Array.isArray(ed)) throw new Error("ed is not an array at seted(ed)")
         this.object.ed = ed;
+        return this;
     }
     
     /*edFull*/
     this.setedFull = function(edFull) {
         if ( !Array.isArray(edFull)) throw new Error("edFull is not an array at setedFull(edFull)")
         this.object.edFull = edFull;
+        return this;
     }
     
     this.addTo = function(NameOfvar) {
