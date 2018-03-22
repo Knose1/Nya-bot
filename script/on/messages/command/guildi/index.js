e941ca0b03736e5b1c9e16017a01b6ce50975050
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-22 13:16:39 +0100

    Update index.js

--- script/on/messages/command/guildi/index.js
+++ script/on/messages/command/guildi/index.js
@@ -1,18 +1,33 @@
 exports.execute = () => {
 
     //Commande GUILDI
-        if (command == 'guildi' && message.author == botowner) {
+        if (message.author == botowner) {
+            
             var guild = client.guilds.get(args[0]);
             if (guild) {
                 if (guild.available) {
-                    guild.fetchInvites(invites => {
-                    if (invites.length > 0) {
-                        guild.fetchInvites().then(returned => message.author.send(returned.first().toString()) );
-                    } else {
-                        message.author.send('No invite for the server '+ guild.name)
+                    
+                    var options_nyach = {
+                        permissions: ["CREATE_INSTANT_INVITE"],
+                        message: message,
+                        guild: guild,
+                        author: client.user
                     }
-                });
-            } else message.channel.send(`${args[0]} is not a guild`);
+                    var perm_nyach = new check_perm(options_nyach);
+                    
+                    perm_nyach.check().then(nnya => {
+                        //if (nnya) {
+                            guild.fetchInvites(invites => {
+                                if (invites.length > 0) {
+                                    message.author.send(invites.first().toString());
+                                } else {
+                                    message.author.send('No invite for the server '+ guild.name)
+                                }
+                            });
+                        //} else message.author.send(`I don't have CREATE_INSTANT_INVITE in "${guild.name}"`)
+                    });
+                }
+            } else message.author.send(`${args[0]} is not a guild`);
         }
 
 }
