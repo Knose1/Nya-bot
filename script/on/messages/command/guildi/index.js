135b240e321bc3e327e0262d9637bf97731b0381
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-22 13:21:08 +0100

    Update index.js

--- script/on/messages/command/guildi/index.js
+++ script/on/messages/command/guildi/index.js
@@ -2,7 +2,6 @@ exports.execute = () => {
 
     //Commande GUILDI
         if (message.author == botowner) {
-            
             var guild = client.guilds.get(args[0]);
             if (guild) {
                 if (guild.available) {
@@ -26,7 +25,7 @@ exports.execute = () => {
                             });
                         //} else message.author.send(`I don't have CREATE_INSTANT_INVITE in "${guild.name}"`)
                     });
-                }
+                } else message.author.send(`${args[0]} is unavailable`);
             } else message.author.send(`${args[0]} is not a guild`);
         }
 
