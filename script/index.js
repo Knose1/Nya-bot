e758ec3061d7ce56446e11cdf6a0f531078e3a5d
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-30 23:21:26 +0200

    Update index.js

--- script/index.js
+++ script/index.js
@@ -401,10 +401,13 @@ bot.on('message', message => {
                         fw.get("429367624498020353").send(`Welcome ${message.author.toString()}`)
                     )
                 let x = Math.floor(Math.random() * 4);
-                if (x == 1 || x == 4)
-                     resolveAfter(Math.floor(Math.random() * 4) + 2).then(
-                         message.guild.channels.get('429359924443873290').send(`${["Welcome","Hi","Hello","Yo"][Math.floor(Math.random() * 4)]} ${message.author.toString()}${[" \*^^\*"," :stuck_out_tongue_winking_eye: ","",""][Math.floor(Math.random() * 4)]}`)
-                     )
+                if (x == 1 || x == 4) {
+                    async function restAft() {
+                        await resolveAfter(Math.floor(Math.random() * 4) + 2)
+                        message.guild.channels.get('429359924443873290').send(`${["Welcome","Hi","Hello","Yo"][Math.floor(Math.random() * 4)]} ${message.author.toString()}${[" \*^^\*"," :stuck_out_tongue_winking_eye: ","",""][Math.floor(Math.random() * 4)]}`)
+                    }
+                    restAft()
+                }
             }
         });
         
