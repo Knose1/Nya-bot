ffce0ae9c4db20039ef5644fdb9f645f975e7f1c
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-04-04 21:08:07 +0200

    Create index.js

---
+++ script/on/messages/command/avatar/index.js
@@ -0,0 +1,4 @@
+exports.execute = () => {
+    if(args[0]) if(client.users.find('tag',args[0])) message.channel.send(client.users.find('tag',args[0]).avatarURL);
+    else message.channel.send(message.author.avatarURL);
+}
