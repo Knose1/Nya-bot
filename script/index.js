115c5cf57b28e2b812171776d8686ba5c4cec164
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-04-04 21:57:18 +0200

    Update index.js

--- script/index.js
+++ script/index.js
@@ -542,6 +542,24 @@ bot.on('message', message => {
                 message.delete(500);
                 message.channel.send(message.content.slice("CD_".length));
             
+            } else if (message.content.indexOf("!CD_purge") == 0) {
+                
+                if (message.content.slice("!CD_purge".length).length != 0) {
+                    if ("NaN" != String(Number( message.content.slice("!CD_purge".length) )) ) {
+                        if ( Math.floor( Number(message.content.slice("!CD_purge".length)) ) <= 100 && Math.floor( Number(message.content.slice("!CD_purge".length)) ) > 0 ) {
+                            message.delete(500);
+                            message.channel.fetchMessages({ limit: Math.floor( Number(message.content.slice("!CD_purge".length)) ) }).then(ms => ms.forEach( m => {
+                                m.delete(10);
+                            }));
+                        } else {
+                            message.channel.send("Please enter a number between 1 and 100").then(m => m.delete(8000))
+                        }
+                    } else {
+                        message.channel.send("Please enter a number to purge").then(m => m.delete(8000))
+                    }
+                } else {
+                    message.channel.send("Please enter a number to purge").then(m => m.delete(8000))
+                }
             }
         
         }
