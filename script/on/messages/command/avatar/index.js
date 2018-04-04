0507a882ccbce18929b76aee82c6abcf5542af42
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-04-04 21:11:40 +0200

    Update index.js

--- script/on/messages/command/avatar/index.js
+++ script/on/messages/command/avatar/index.js
@@ -1,4 +1,7 @@
 exports.execute = () => {
-    if(args[0]) if(client.users.find('tag',args[0])) message.channel.send(client.users.find('tag',args[0]).avatarURL);
-    else message.channel.send(message.author.avatarURL);
+    if(args[0]) {
+        if(client.users.find('tag',args[0])) {
+            message.channel.send(client.users.find('tag',args[0]).avatarURL);
+        }
+    } else message.channel.send(message.author.avatarURL);
 }
