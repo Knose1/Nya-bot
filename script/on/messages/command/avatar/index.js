bf659d767c09263eeb7f9e2015cffd3f8da31df2
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-04-06 10:49:03 +0200

    Update index.js

--- script/on/messages/command/avatar/index.js
+++ script/on/messages/command/avatar/index.js
@@ -1,18 +1,20 @@
 exports.execute = () => {
-    if(args[0]) {
-        if (message.mentions) {
-            if (message.mentions.users.array().length > 0) {
-                message.channel.send(message.mentions.users.first().avatarURL);
+    try {
+        if(args[0]) {
+            if (message.mentions) {
+                if (message.mentions.users.array().length > 0) {
+                    message.channel.send(message.mentions.users.first().avatarURL);
+                } else if(client.users.find('tag',args[0])) {
+                    message.channel.send(client.users.find('tag',args[0]).avatarURL);
+                } else if(client.users.find('username',args[0])) {
+                    message.channel.send(client.users.find('tag',args[0]).avatarURL);
+                }
+            
             } else if(client.users.find('tag',args[0])) {
                 message.channel.send(client.users.find('tag',args[0]).avatarURL);
             } else if(client.users.find('username',args[0])) {
                 message.channel.send(client.users.find('tag',args[0]).avatarURL);
             }
-        
-        } else if(client.users.find('tag',args[0])) {
-            message.channel.send(client.users.find('tag',args[0]).avatarURL);
-        } else if(client.users.find('username',args[0])) {
-            message.channel.send(client.users.find('tag',args[0]).avatarURL);
-        }
-    } else message.channel.send(message.author.avatarURL);
+        } else message.channel.send(message.author.avatarURL);
+    } catch (err) {}
 }
