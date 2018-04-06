a8a928b573c4a20677d170f7dacbe72d3baa9321
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-04-06 09:40:50 +0200

    Update index.js

--- script/on/messages/command/avatar/index.js
+++ script/on/messages/command/avatar/index.js
@@ -1,6 +1,14 @@
 exports.execute = () => {
     if(args[0]) {
+        if (message.mentions) {
+            if (message.mentions.users.array().length > 0) {
+                message.channel.send(message.mentions.users.first().avatarURL);
+                return;
+            }
+        }
         if(client.users.find('tag',args[0])) {
+            message.channel.send(client.users.find('tag',args[0]).avatarURL);
+        } else if(client.users.find('username',args[0])) {
             message.channel.send(client.users.find('tag',args[0]).avatarURL);
         }
     } else message.channel.send(message.author.avatarURL);
