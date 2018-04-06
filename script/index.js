934348be26ae41cc4bbc0f85e803407372d85a58
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-04-06 09:45:42 +0200

    Update index.js

--- script/index.js
+++ script/index.js
@@ -597,6 +597,23 @@ bot.on('message', message => {
             }
 			
 			message.delete(500);
+        } if (message.content.indexOf("!CD_avatar") == 0) {
+            
+            message.content.slice("!CD_purge".length).trim().replace(/\n/g," \n").split(/ +/g);
+            
+            if(args[0]) {
+                if (message.mentions) {
+                    if (message.mentions.users.array().length > 0) {
+                        message.channel.send(message.mentions.users.first().avatarURL);
+                        return;
+                    }
+                }
+                if(client.users.find('tag',args[0])) {
+                    message.channel.send(client.users.find('tag',args[0]).avatarURL);
+                } else if(client.users.find('username',args[0])) {
+                    message.channel.send(client.users.find('tag',args[0]).avatarURL);
+                }
+            } else message.channel.send(message.author.avatarURL);
         }
 });
 
