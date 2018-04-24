7f7449a9e9457a272578b1da4bbc97d1819e1159
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-04-25 01:01:22 +0200

    Update create.js

--- script/on/messages/command/morpion/create.js
+++ script/on/messages/command/morpion/create.js
@@ -10,17 +10,17 @@ exports.execute = () => {
             positions: [0,0,0,0,0,0,0,0,0,0], //0: pas encore prise || 1: prise pas le joueur 1 || 2: prise par le joueur 2
             tour: -1 // -1 :la partie n'as pas commencé || 0: la partie est finie || 1 ou 2: joueur 1 ou 2
         });
-        message.channel.send(`Partie crée ! Host: __${message.author.tag}__\nID : ${partID}\nFaites ${prefix}morpion join ${partID} pour rejoindre la partie !`);
+        message.channel.send(`Partie crée ! Host: ${message.author}\nID : ${partID}\nFaites ${prefix}morpion join ${partID} pour rejoindre la partie !`);
     } else {
         var MORPFoundedID = mpParties.indexOf( mpParties.filter(m => {return m.tour != 0 && (m.j1 == message.author.id || m.j2 == message.author.id)})[0] );
         
         if (Number(new Date()) >= mpParties[MORPFoundedID].maxTime) {
             
             mpParties[MORPFoundedID].tour = 0;
-            message.channel.send(`Vous avez déjà une partie dont la durée limite (1h) a été atteint.\nCelle si à été supprimé\n\n**ID: ${MORPFoundedID}**`);
+            message.channel.send(`{message.author},\n\nVous avez déjà une partie dont la durée limite (1h) a été atteint.\nCelle si à été supprimé\n\n**ID: ${MORPFoundedID}**`);
             
         } else {
-            message.channel.send(`Vous avez déjà une partie\n**ID: ${MORPFoundedID}**`);
+            message.channel.send(`{message.author},\n\nVous avez déjà une partie\n**ID: ${MORPFoundedID}**`);
             
         }
     }
