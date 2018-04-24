a1e045696b4f8f34d0245a6014d1441e4bc64b99
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-04-25 01:24:27 +0200

    Update join.js

--- script/on/messages/command/morpion/join.js
+++ script/on/messages/command/morpion/join.js
@@ -3,7 +3,7 @@ exports.execute = () => {
     //On regarde s'il n'existe aucune partie en cours
     if ( mpParties.filter(m => {return m.tour != 0 && (m.j1 == message.author.id || m.j2 == message.author.id)}).length == 0 ) {
         if (args[1]) {
-            if (args[1].replace(/0/g,"").replace(/1/g,"").replace(/2/g,"").replace(/3/g,"").replace(/4/g,"").replace(/5/g,"").replace(/6/g,"").replace(/7/g,"").replace(/8/g,"").replace(/9/g,"").lenght == 0 ) {
+            if (args[1].replace(/0/g,"").replace(/1/g,"").replace(/2/g,"").replace(/3/g,"").replace(/4/g,"").replace(/5/g,"").replace(/6/g,"").replace(/7/g,"").replace(/8/g,"").replace(/9/g,"").length == 0 ) {
                 if (mpParties[args[1]]) {
                     if (mpParties[args[1]].j1 != message.author.id) {
                         if (mpParties[args[1]].j2 == false) {
@@ -12,11 +12,11 @@ exports.execute = () => {
                             eval(`var morpFirstPlayer = mpParties[args[1]].j${rand(1,2)}`);
                             message.channel.send(`<@${mpParties[args[1]].j1}> VS <@${mpParties[args[1]].j2}> || C'est <@${morpFirstPlayer}> qui commence !\nVoici les positions:\n\n7 | 8 | 9\n4 | 5 | 6\n1 | 2 | 3`)
                         
-                        } else message.channel.send(`{message.author},\n\nLa partie est déjà complète`);
-                    } else message.channel.send(`{message.author},\n\nVous êtes déjà dans la partie`);
-                }else message.channel.send(`{message.author},\n\nLa partie ${args[1]} n'existe pas`);
-            } else message.channel.send(`{message.author},\n\nL'id doit être un nombre`);
-        } else message.channel.send(`{message.author},\n\nUtilisation: \`cat:morpion join <id>\``);
+                        } else message.channel.send(`${message.author},\n\nLa partie est déjà complète`);
+                    } else message.channel.send(`${message.author},\n\nVous êtes déjà dans la partie`);
+                }else message.channel.send(`${message.author},\n\nLa partie ${args[1]} n'existe pas`);
+            } else message.channel.send(`${message.author},\n\nL'id doit être un nombre`);
+        } else message.channel.send(`${message.author},\n\nUtilisation: \`cat:morpion join <id>\``);
         
         
         
