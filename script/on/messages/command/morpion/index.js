eb95f92403958b4ace788737a85f74ed256cd50d
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-04-24 23:58:04 +0200

    Update index.js

--- script/on/messages/command/morpion/index.js
+++ script/on/messages/command/morpion/index.js
@@ -4,7 +4,7 @@ exports.execute = () => {
     
     if (args[0])
         if (availableSubcommands.indexOf(args[0].toLowerCase()) > -1) {
-            var funcSubComm = String(require(`./args[0].js`).execute);
+            var funcSubComm = String(require(`./${args[0]}.js`).execute);
             var toEvSC = funcSubComm.slice(7, funcSubComm.length - 1)/*.replace(/\n/g,"").replace(/ +/g," ")*/;
             //console.log(toEvSC);
             eval(toEvSC);
