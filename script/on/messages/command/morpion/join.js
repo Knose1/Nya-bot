26ccf9799b319687a6f03187ad743dc8bfb5ef7f
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-04-25 12:10:00 +0200

    Update join.js

--- script/on/messages/command/morpion/join.js
+++ script/on/messages/command/morpion/join.js
@@ -5,7 +5,7 @@ exports.execute = () => {
         if (args[1]) {
             if (args[1].replace(/0/g,"").replace(/1/g,"").replace(/2/g,"").replace(/3/g,"").replace(/4/g,"").replace(/5/g,"").replace(/6/g,"").replace(/7/g,"").replace(/8/g,"").replace(/9/g,"").length == 0 ) {
                 if (mpParties[args[1]]) {
-                    if (mpParties[args[1]].j1 != message.author.id) {
+                    if (mpParties[args[1]].tour != 0) {
                         if (mpParties[args[1]].j2 == false) {
                             
                             mpParties[args[1]].j2 = message.author.id;
@@ -14,7 +14,7 @@ exports.execute = () => {
                             message.channel.send(`<@${mpParties[args[1]].j1}> VS <@${mpParties[args[1]].j2}> || C'est <@${morpFirstPlayer}> qui commence !\nVoici les positions:\n\n7 | 8 | 9\n4 | 5 | 6\n1 | 2 | 3`)
                         
                         } else message.channel.send(`${message.author},\n\nLa partie est déjà complète`);
-                    } else message.channel.send(`${message.author},\n\nVous êtes déjà dans la partie`);
+                    }else message.channel.send(`${message.author},\n\nLa partie ${args[1]} a été supprimé`)
                 }else message.channel.send(`${message.author},\n\nLa partie ${args[1]} n'existe pas`);
             } else message.channel.send(`${message.author},\n\nL'id doit être un nombre`);
         } else message.channel.send(`${message.author},\n\nUtilisation: \`cat:morpion join <id>\``);
