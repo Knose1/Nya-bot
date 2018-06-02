b223ec9e7001ae47cefa298de801c1d104abd24d
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-06-02 09:41:20 +0200

    Update index.js

--- script/on/messages/command/avatar/index.js
+++ script/on/messages/command/avatar/index.js
@@ -10,10 +10,12 @@ exports.execute = () => {
                     message.channel.send(client.users.find('tag',args[0]).avatarURL);
                 }
             
-            } else if(client.users.find('tag',args[0])) {
-                message.channel.send(client.users.find('tag',args[0]).avatarURL);
-            } else if(client.users.find('username',args[0])) {
-                message.channel.send(client.users.find('tag',args[0]).avatarURL);
+            } else if(client.users.find('tag',args.join(" "))) {
+                message.channel.send(client.users.find('tag',args.join(" ")).avatarURL);
+            } else if(client.users.find('username',args.join(" "))) {
+                message.channel.send(client.users.find('username',args.join(" ")).avatarURL);
+            } else {
+                message.channel.send("Sorry I can't find " + args.join(" "))
             }
         } else message.channel.send(message.author.avatarURL);
     } catch (err) {}
