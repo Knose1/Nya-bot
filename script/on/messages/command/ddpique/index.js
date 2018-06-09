374a17f1d5f8e82be760000bfd4fbfdc58f51e22
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-06-09 18:10:14 +0200

    Create index.js

---
+++ script/on/messages/command/ddpique/index.js
@@ -0,0 +1,13 @@
+exports.execute = () => {
+
+    let availableSubcommands = ["delete","join","create","play"];
+    
+    if (args[0])
+        if (availableSubcommands.indexOf(args[0].toLowerCase()) > -1) {
+            var funcSubComm = String(require(`./on/messages/command/ddpique/${args[0]}.js`).execute);
+            var toEvSC = funcSubComm.slice(7, funcSubComm.length - 1)/*.replace(/\n/g,"").replace(/ +/g," ")*/;
+            //console.log(toEvSC);
+            eval(toEvSC);
+        }
+
+}
