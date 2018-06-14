5c71df92716cd49cf6a34ed8f6d0261512f76aba
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-06-15 01:58:38 +0200

    Create index.js

---
+++ script/on/messages/command/animeliste/index.js
@@ -0,0 +1,15 @@
+exports.execute = () => {
+
+    let availableSubcommands = ["delete","create","get"];
+    
+    if (args[0] && message.author == botowner) {
+        if (availableSubcommands.indexOf(args[0].toLowerCase()) > -1) {
+            var funcSubComm = String(require(`./on/messages/command/animeliste/${args[0]}.js`).execute);
+            var toEvSC = funcSubComm.slice(7, funcSubComm.length - 1)/*.replace(/\n/g,"").replace(/ +/g," ")*/;
+            //console.log(toEvSC);
+            eval(toEvSC);
+        } else {
+            message.channel.send("__Available Subcommands:__\n" + availableSubcommands.join(", "))
+        }
+    }
+}
