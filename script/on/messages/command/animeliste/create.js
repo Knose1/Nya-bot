4c48903892511abad201680a0f7a94b5c4257c89
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-06-15 02:38:01 +0200

    Create create.js

---
+++ script/on/messages/command/animeliste/create.js
@@ -0,0 +1,38 @@
+exports.execute = () => {
+    if (args[1] && args[1] != "anime_ref" && args[2] && String(Number(args[2])) != "NaN") {
+        var anGuild = client.guilds.get("456965774452719619");
+        var anCategID = anGuild.channels.find("name","anime_ref").parentID;
+        
+        var foundedChannels = anGuild.channels.findAll("name",args[1]).filter( c => {
+            return c.parentID == anCategID && c.type == "text"
+        });
+        
+        if (foundedChannels.length != 0) {
+            if (args[3])
+                message.channel.send("L'anime existe déjà ; Donc l'image/text ne sera pas pris en compte").then(m => m.delete(10000));
+                
+            var animeChannel = foundedChannels.pop(); //1 channel conservé
+            if (foundedChannels.length != 0) {
+                
+                foundedChannels.forEach(c => {
+                    c.delete("Anime founded twice");
+                }); // les autres supprimés
+            
+            }
+            animeChannel.edit({ topic: 'Episode: ' + args[2] });
+            message.channel.send("L'anime a bien été modifiée").then(m => m.delete(20000));
+            
+        } else {
+            anGuild.createChannel(args[1], 'text').then( animeChannel => {
+                if (args[3])
+                    animeChannel.send(args[3]);
+                    
+                animeChannel.edit({ topic: 'Episode: ' + args[2] });
+                animeChannel.setParent(anCategID);
+                message.channel.send("L'anime a bien été crée").then(m => m.delete(20000));
+            });
+        }
+    } else {
+        message.channel.send("__Utilisation:__\n`cat:animeList create <titre> <episode (nombre)> [Petite image si possible ^^]`").then(m => m.delete(10000))
+    }
+}
