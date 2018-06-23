c1b0ad916605012ad98414c742030b4f587a2e40
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-06-23 12:03:03 +0200

    Update index.js

--- script/on/messages/command/logserv/index.js
+++ script/on/messages/command/logserv/index.js
@@ -3,7 +3,7 @@ exports.execute = () => {
     //Commande LOGSERV
         if (command == 'logserv') {
             message.channel.send(`Voici le server log: \n https://discord.gg/HTZy7tB`);
-            console.log('Log Invitation envoyé');
+            Nya.log('Log Invitation envoyé');
             var channel = client.channels.get(logserv);
             channel.send('Log Invitation envoyé');
         }
