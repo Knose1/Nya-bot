98185055c1b4d81c63f2fee4feb54b8c631cc9aa
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-06-26 00:03:23 +0200

    Update finaly.js

--- script/on/messages/vs/finaly.js
+++ script/on/messages/vs/finaly.js
@@ -296,6 +296,46 @@ exports.execute = async function(message, isVs, Pfx) {
         if (message.attachments.size != 0) {
             await resolveAfter(5);
         }
+        
+        
+        var allNya = client.channels.filter(f => {return (f.name == "nya-bot-vs-" + Pfx.name || f.name == "nya-bot-vs" && Pfx.name == "") && f.type == "text" && f.name != "nya-bot-vs-log"})
+        if (vsStatus[message.author.id]) {
+            
+            var oldVsStatus = vsStatus[message.author.id];
+            vsStatus[message.author.id] = undefined
+        
+            //On créer l'embed
+            
+            var nbmois = new Date().getMonth();
+            nbmois = nbmois+1;
+            const embed = new Discord.RichEmbed()
+                //.setTitle("Virtual Channel")
+                .setAuthor(message.author.username+"#"+message.author.discriminator /*, message.author.avatarURL*/)
+                .setColor("#FFFFFF")
+                .setDescription(`Has no more the status "${oldVsStatus}"`)
+                .setFooter("Le "+ UTCDate(Pfx.UTC).getDate()+"/"+ nbmois+"/"+ UTCDate(Pfx.UTC).getFullYear()+" à "+ UTCDate(Pfx.UTC).toLocaleTimeString()+` (UTC+${Pfx.UTC}) | `+message.guild.name.replace(/`/g,"").replace(/_/g,"").replace(/\*/g,"")+" | "+message.author.id , message.guild.iconURL)
+                .setThumbnail(message.author.avatarURL);
+            
+            allNya.forEach( n => n.send({embed}).then(d => d.delete(20000)) )
+        }
+        
+        message.mentions.members.map(m => m.id).forEach(m => {
+            if (vsStatus[m]) {
+                //On créer l'embed
+            
+                var nbmois = new Date().getMonth();
+                nbmois = nbmois+1;
+                const embedStatus = new Discord.RichEmbed()
+                    //.setTitle("Virtual Channel")
+                    .setAuthor(message.author.username+"#"+message.author.discriminator /*, message.author.avatarURL*/)
+                    .setColor("#FFFFFF")
+                    .setDescription("__Status:__\n\n" + vsStatus[m])
+                    .setFooter("Le "+ UTCDate(Pfx.UTC).getDate()+"/"+ nbmois+"/"+ UTCDate(Pfx.UTC).getFullYear()+" à "+ UTCDate(Pfx.UTC).toLocaleTimeString()+` (UTC+${Pfx.UTC}) | `+message.guild.name.replace(/`/g,"").replace(/_/g,"").replace(/\*/g,"")+" | "+message.author.id , message.guild.iconURL)
+                    .setThumbnail(message.author.avatarURL);
+                
+            allNya.forEach( n => n.send({embedStatus}).then(d => d.delete(20000)) )
+            }
+        });
         message.delete(1000)
             .then(msg => Nya.log(`Message supprimé, raison: Virtual channel; Auteur: ${msg.author}`))
             .catch(Nya.error);
