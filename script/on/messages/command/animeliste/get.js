77f23e67b84b6f136134a569578b31322ac8a45c
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-06-29 01:38:09 +0200

    Update get.js

--- script/on/messages/command/animeliste/get.js
+++ script/on/messages/command/animeliste/get.js
@@ -5,6 +5,6 @@ exports.execute = () => {
     var foundedChannels = anGuild.channels.array().filter( c => {
         return c.name != "anime_ref" && c.parentID != anGuild.channels.find("name","général").parentID && c.type == "text"
     });
-    message.channel.send(`${botowner}\n\n\n` + foundedChannels.map(m => {return try {m + " " + m.topic} catch(e) {}}).join("\n\n"), {split:true}).then(m => m.delete(30000));
+    message.channel.send(`${botowner}\n\n\n` + foundedChannels.map(m => {try {return m + " " + m.topic} catch(e) {}}).join("\n\n"), {split:true}).then(m => m.delete(30000));
     
 }
