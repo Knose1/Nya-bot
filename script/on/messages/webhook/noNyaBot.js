650cf09c7a99aac5261ea5213282c08412076f38
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-07-02 20:12:53 +0200

    Update noNyaBot.js

--- script/on/messages/webhook/noNyaBot.js
+++ script/on/messages/webhook/noNyaBot.js
@@ -1,33 +1,6 @@
 exports.execute = () => {
-    
-    message.channel.fetchWebhooks().then(fw => {
-        
-        try {
-        
-        if (fw.find('name', 'NoNya!Bot') != undefined)
-            NoNyaWebhooks = true;
-        else
-            NoNyaWebhooks = false;
-        } catch (e) {
-            NoNyaWebhooks = false;
-        }
-            
-        var options_pch2 = {
-            permissions: ["MANAGE_CHANNELS","MANAGE_WEBHOOKS"],
-            message: message,
-        }
-        
-        check_perm(options_pch2)().then(pprm => {
-            
-            //Si ce n'est pas le nya!bot , qu'il n'as pas les perms et que le webhook est activé : Bang Bang ! Tu ne poourra pas acceder à la commande
-            if (NoNyaWebhooks && !pprm && !(message.author.id == client.user.id) )
-                return;
-
-            
-            
-            
-            
-            //On récupère le suffix du vs
+    function messDefault() {
+        //On récupère le suffix du vs
             var isVs = false;
             if (message.channel.name.indexOf('nya-bot-vs') == 0) {
                 var Pfx = message.channel.name.slice('nya-bot-vs'.length);
@@ -285,6 +258,39 @@ exports.execute = () => {
                 }
                 iscommand = false;
             }
+    }
+    
+    
+    
+    message.channel.fetchWebhooks().then(fw => {
+        
+        try {
+        
+        if (fw.find('name', 'NoNya!Bot') != undefined)
+            NoNyaWebhooks = true;
+        else
+            NoNyaWebhooks = false;
+        } catch (e) {
+            NoNyaWebhooks = false;
+        }
+            
+        var options_pch2 = {
+            permissions: ["MANAGE_CHANNELS","MANAGE_WEBHOOKS"],
+            message: message,
+        }
+        
+        check_perm(options_pch2)().then(pprm => {
+            
+            //Si ce n'est pas le nya!bot , qu'il n'as pas les perms et que le webhook est activé : Bang Bang ! Tu ne poourra pas acceder à la commande
+            if (NoNyaWebhooks && !pprm && !(message.author.id == client.user.id) )
+                return;
+
+                messDefault()            
+            
+            
+            
+            
         }); //Fin promise (Permissions)
-    }); //Fin promise Webhook
+    }) //Fin promise Webhook
+    .catch(messDefault())
 }
