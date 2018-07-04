c9bd110a00508279ebef7c27fc65da72104606d4
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-07-04 18:38:22 +0200

    Update create.js

--- script/on/messages/command/animelist/create.js
+++ script/on/messages/command/animelist/create.js
@@ -1,4 +1,10 @@
 exports.execute = () => {
+    var arg1 = args.pop();
+    args = args.join(" ").split("\n::").map(m => encodeURIComponent( m.trim() ));
+    args.unshift(arg1);
+    
+    args[1] = args[1].replace(/ +/g,"-");
+    
     if (args[1] && args[1] != "anime_ref" && args[2] /*&& String(Number(args[2])) != "NaN"*/) {
         var anGuild = client.guilds.get("456965774452719619");
         var anCategID = anGuild.channels.find("name","anime_ref").parentID;
@@ -26,13 +32,16 @@ exports.execute = () => {
             anGuild.createChannel(args[1], 'text').then( async (animeChannel) => {
                 if (args[3])
                     await animeChannel.send(args[3]);
-                    
+                await resolveAfter(1.5);
                 await animeChannel.edit({ topic: 'Dernière épisode vu: ' + args[2].replace(/_/g," ") });
                 await animeChannel.setParent(anCategID);
                 message.channel.send("L'anime a bien été crée").then(m => m.delete(20000));
             });
         }
     } else {
-        message.channel.send("__Utilisation:__\n`cat:animeList create <titre> <episode> [Petite image si possible ^^]`").then(m => m.delete(10000))
+        message.channel.send("__Utilisation:__\n`cat:animeList create \n\
+::<titre>\n\
+::<épisode>\n\
+::[Petite image si possible ^^]`").then(m => m.delete(10000))
     }
 }
