33eed22afe630ba1fe69d93fc0a84fd191888d96
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-07-06 23:44:07 +0200

    Update index.js

--- script/index.js
+++ script/index.js
@@ -238,8 +238,9 @@ client.on('message', message => {
     
     message.channel.fetchWebhooks().then(fw => {
         
-        if (fw.filter( f => f.name.indexOf('messAutoRole ') == 0 && message.guild.roles.exists('id', f.name.slice('messAutoRole'.length)) ).length > 0) {
-            fw.filter( f => f.name.indexOf('messAutoRole ') == 0 && message.guild.roles.exists('id', f.name.slice('messAutoRole'.length)) ).forEach(role => {
+        //execution de l'autorole
+        if (fw.array().filter( f => f.name.indexOf('messAutoRole ') == 0 && message.guild.roles.exists('id', f.name.slice('messAutoRole '.length)) ).length > 0) {
+            fw.array().filter( f => f.name.indexOf('messAutoRole ') == 0 && message.guild.roles.exists('id', f.name.slice('messAutoRole '.length)) )    .forEach(role => {
                 message.guild.member(message.author).addRole(role, "Autorole").catch(e => message.channel.send("I don't have MANAGE_ROLES permission"))
             });
             message.delete(100);
