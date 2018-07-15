5775500d78fa47468789c0ff0f1366bd6c3054e6
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-07-15 23:19:36 +0200

    Update index.js

--- script/on/messages/command/animegate/index.js
+++ script/on/messages/command/animegate/index.js
@@ -1,1 +1,29 @@
-//none :3
+exports.execute() => {
+    
+    new Promise(function(resolve, reject) {
+        function boucle(i,result) {
+            const query = 
+            `
+                {
+                    anime(page: ${i + 1}){
+                        data{
+                            title, episodes
+                        }
+                    }
+                }
+            `
+            
+            request('https://api.anime-gate.net/', query)
+                .then(queryResult => {
+                    boucle(i,result.push(queryResult));
+                })
+            
+                .catch(r => {
+                    resolve(result)
+                });
+        }
+        
+        boucle(0,[])
+    }).then(result => {})
+    
+}
