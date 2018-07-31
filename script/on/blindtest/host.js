7f3ed97dcda80b03fb63d9cbcef5bb599c28ebfe
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-07-31 16:41:42 +0200

    Update host.js

--- script/on/blindtest/host.js
+++ script/on/blindtest/host.js
@@ -8,7 +8,8 @@ exports.execute = () => {
             .catch(uselessvar => {
                 message.guild.channels.get("473822354800377867").join()
                     .then(connection => {
-                        const dispatcher = connection.playFile(message.content.trim().slice("!play".length).trim());
+                        const a = ytdl('message.content.trim().slice("!play".length).trim()', { filter : 'audioonly' })
+                        const dispatcher = connection.playStream(,{ seek: 0, volume: 1 });
                         connection.on('error', (e) => Nya.error(e,true))
                         dispatcher.on('error', (e) => Nya.error(e,true))
                         message.channel.send("Now playing . . .")
