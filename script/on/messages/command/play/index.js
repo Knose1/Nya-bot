0c717722c350e41b74c76c4372e36476a92cd751
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-07-31 18:04:25 +0200

    Update index.js

--- script/on/messages/command/play/index.js
+++ script/on/messages/command/play/index.js
@@ -1,20 +1,21 @@
 exports.execute = () => {
-    if (message.member.voiceChannel) {
-        message.member.voiceChannel.leave()
+    if (!args[0]) {
+        message.reply("syntax: cat:play <url> (replace <url> by his value)")
+    } else if (message.member.voiceChannel) {
+        //message.member.voiceChannel.leave()
         message.member.voiceChannel.join()
-            .then(connection => {
-                const link = args[0]    
+            .then(connection => {     
                 const stream = require('stream');    
 
-                if (link.indexOf("https://www.youtube.com/watch?v=") == 0 || link.indexOf("https://youtu.be/") == 0) {
+                if (args[0].indexOf("https://www.youtube.com/watch?v=") == 0 || args[0].indexOf("https://youtu.be/") == 0) {
                     var steamAudio = ytdl(message.content.trim().slice("!play".length).trim(), { filter : 'audioonly' })
                     var dispatcher = connection.playStream(steamAudio,{ seek: 0, volume: 1 });
                 } else {
-                    var dispatcher = connection.playArbitraryInput(link);
+                    var dispatcher = connection.playArbitraryInput(args[0]);
                 }
                 connection.on('error', (e) => Nya.error(e,true))
                 dispatcher.on('error', (e) => Nya.error(e,true))
-                message.channel.send("Now playing " + link)
+                message.channel.send("Now playing " + args[0])
             })
             .catch(e => Nya.error(e,true));
         
