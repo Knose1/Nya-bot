c54c39d3b4378629ba0ef94ab4e9c681a4eb93db
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-07-31 19:27:07 +0200

    Update index.js

--- script/on/messages/command/serv/index.js
+++ script/on/messages/command/serv/index.js
@@ -1,5 +1,8 @@
 exports.execute = () => {
-
+    message.delete(500)
+        .then(msg => Nya.log(`Message supprimé, raison: commande; Auteur: ${msg.author}`))
+        .catch(e => Nya.error(e,true));
+    
     //Commande Serv
         if ((command == 'guilds' || command == 'serv') && message.author == botowner) {
         var nyaguilds = '__Serveurs:__ \n\n';
