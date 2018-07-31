3f66f2b48c979e23e2c19782f67d8398d6ed455a
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-07-31 18:20:04 +0200

    Update index.js

--- script/on/messages/command/stop/index.js
+++ script/on/messages/command/stop/index.js
@@ -1,6 +1,7 @@
 exports.execute = () => {
     if (message.member.voiceChannel) {
         message.member.voiceChannel.leave()
+        message.channel.send("Successfully stoped the music")
     } else {
         message.reply('You need to join a voice channel first!');
     }
