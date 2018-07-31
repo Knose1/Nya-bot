1ffc4726d1334b0710556e172dd47e8116a56568
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-07-31 15:21:08 +0200

    Update host.js

--- script/on/blindtest/host.js
+++ script/on/blindtest/host.js
@@ -1,1 +1,18 @@
-
+exports.execute = () {
+    if (message.content.trim().toLowerCase().indexOf("!play") == 0) {
+        message.guild.channels.get("473838075999420427").send({files:[{
+            attachment:message.content.trim().slice("!play".length).trim(),
+            name:"Guess"
+        }]})
+            .then( uselessvar => message.channel.send("File uploaded in <#473818364477833216>") )
+            .catch(uselessvar => {
+                message.guild.channels.get("473822354800377867").join()
+                    .then(connection => {
+                        const dispatcher = connection.playFile(message.content.trim().slice("!play".length).trim());
+                        message.channel.send("Now playing . . .")
+                    })
+                    .catch(e => Nya.error(e,true));
+            })
+    }
+    
+}
