796573fe500c5a0709642b62798208cd24fe4455
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-07-31 17:17:00 +0200

    Update host.js

--- script/on/blindtest/host.js
+++ script/on/blindtest/host.js
@@ -8,8 +8,15 @@ exports.execute = () => {
             .catch(uselessvar => {
                 message.guild.channels.get("473822354800377867").join()
                     .then(connection => {
-                        const steamAudio = ytdl('message.content.trim().slice("!play".length).trim()', { filter : 'audioonly' })
-                        const dispatcher = connection.playStream(steamAudio,{ seek: 0, volume: 1 });
+                        const link = message.content.trim().slice("!play".length).trim().split(/ +/g)[0]    
+                        const stream = require('stream');    
+                    
+                        if (link.indexOf("https://www.youtube.com/watch?v=") == 0 || link.indexOf("https://youtu.be/") == 0) {
+                            var steamAudio = ytdl(message.content.trim().slice("!play".length).trim(), { filter : 'audioonly' })
+                            var dispatcher = connection.playStream(steamAudio,{ seek: 0, volume: 1 });
+                        } else {
+                            var dispatcher = connection.playArbitraryInput(link);
+                        }
                         connection.on('error', (e) => Nya.error(e,true))
                         dispatcher.on('error', (e) => Nya.error(e,true))
                         message.channel.send("Now playing . . .")
