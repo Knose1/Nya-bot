a7765489539ff29d1e2c72c75cdfb7948f2f0a37
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-09-04 16:19:33 +0200

    Update error_command.js

--- script/on/messages/vs/error_command.js
+++ script/on/messages/vs/error_command.js
@@ -1,10 +1,10 @@
 exports.execute = (message, isVs, iscommand) => {
     //Si (Commande Nya!bot)
     if ((isVs || (message.guild.id == "377892426569744387" && message.channel.name == "nya-bot-vs-log")) && iscommand == true) {
-        message.author.send(message.author+' les commandes sont interdits dans se channel');
-        message.delete(500)
+        message.channel.send(message.author+' les commandes sont interdits dans se channel');
+        /*message.delete(500)
                 .then(msg => Nya.log(`Message supprimé, raison: Virtual channel; Auteur: ${msg.author}`))
-                .catch(Nya.error);
+                .catch(Nya.error);*/
         return true;
     }
     return false;
