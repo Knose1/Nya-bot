d450da776850d171932b38c1b872f8a752c0c917
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-09-04 16:19:54 +0200

    Update error_userBan.js

--- script/on/messages/vs/error_userBan.js
+++ script/on/messages/vs/error_userBan.js
@@ -3,9 +3,9 @@ exports.execute = (message, isVs, isbanned) => {
     //Si (Personne Ban)
     if (isVs && isbanned == true  && message.author.id != mention  && message.author != botowner) {
         message.author.sendMessage(message.author+' vous êtes ban du Virtual server et ne pouvez donc pas parler dans le VS');
-        message.delete(500)
+        /*message.delete(500)
                 .then(msg => Nya.log(`Message supprimé, raison: Virtual channel; Auteur: ${msg.author}`))
-                .catch(Nya.error);
+                .catch(Nya.error);*/
         return true;
     }
     else return false;
