c006bdc2a50951df87d2429c33c090ca4c17470c
Knose1 <34002600+Knose1@users.noreply.github.com>
2019-07-11 21:45:00 +0200

    Update index.js

--- script/on/messages/command/avatarid/index.js
+++ script/on/messages/command/avatarid/index.js
@@ -2,6 +2,8 @@ exports.execute = () => {
 	if (args[0]) {
 		client.fetchUser(args[0]).then( (pUser) => {
 			message.channel.send(pUser.avatarURL);
+		}).catch( () => {
+			message.channel.send("Unable to find id "+args[0]);
 		});
 	}
 }
