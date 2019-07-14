dd404dc5acdd2c95a31563897c90de5c2eb67663
Knose1 <34002600+Knose1@users.noreply.github.com>
2019-07-14 12:35:54 +0200

    Update index.js

--- script/on/messages/command/guildi/index.js
+++ script/on/messages/command/guildi/index.js
@@ -1,14 +1,16 @@
-function tryCreateAnInvite(pChannelList, pGuild, pAuthor, _index = 0) {
-	pChannelList[_index].createInvite({maxAge: 20 * 1000, maxUses:1}).then( (pInvite) => {
-		pAuthor.send(`__**Created an invite for the server ${pGuild.name}:**__\n{String(pInvite)}`);
-	}).catch( (e) => {
-		if (_index + 1 == pChannelList.length)
-			pAuthor.send('There\'s no invite for the server '+ guild.name +'and there\'s no possibility to create an invite');
-		else tryCreateAnInvite(pChannelList, pGuild, pAuthor, ++_index);
-	});
-}
+
 
 exports.execute = () => {
+	let tryCreateAnInvite = (pChannelList, pGuild, pAuthor, _index = 0) => {
+		pChannelList[_index].createInvite({maxAge: 20 * 1000, maxUses:1}).then( (pInvite) => {
+			pAuthor.send(`__**Created an invite for the server ${pGuild.name}:**__\n{String(pInvite)}`);
+		}).catch( (e) => {
+			if (_index + 1 == pChannelList.length)
+				pAuthor.send('There\'s no invite for the server '+ guild.name +'and there\'s no possibility to create an invite');
+			else tryCreateAnInvite(pChannelList, pGuild, pAuthor, ++_index);
+		});
+	}
+	
 	message.delete(500)
 		.then(msg => Nya.log(`Message supprimé, raison: commande; Auteur: ${msg.author}`))
 		.catch(e => Nya.error(e,true));
